e4f27a47fe72d8c99bf90e6028300d716a34de2e
Guillaume WEILL <gweill@guigops.fr>
2024-02-07 18:13:34 +0100

    feat: revue CI

--- pg/lancement.sh
+++ pg/lancement.sh
@@ -1,18 +1,16 @@
 #! /bin/sh
 echo '01-init : '
-psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -f /scripts/01-init.sql
+psql -d ${PGDATABASE} -U ${PGUSER} -f /scripts/01-init.sql
 echo '02-1-geo-init : '
-psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -f /scripts/02/02-1-geo-init.sql
+psql -d ${PGDATABASE} -U ${PGUSER} -f /scripts/02/02-1-geo-init.sql
 echo '02-2-pp-init : '
-psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -f /scripts/02/02-2-doc-init.sql
+psql -d ${PGDATABASE} -U ${PGUSER} -f /scripts/02/02-2-doc-init.sql
 echo '02-3-back-init : '
-psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -f /scripts/02/02-3-back-init.sql
+psql -d ${PGDATABASE} -U ${PGUSER} -f /scripts/02/02-3-back-init.sql
 echo '02-4-front-init : '
-psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -f /scripts/02/02-4-front-init.sql
+psql -d ${PGDATABASE} -U ${PGUSER} -f /scripts/02/02-4-front-init.sql
 echo '03-1-geo-data : '
-psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -f /scripts/03/03-1-geo-data.sql
+psql -d ${PGDATABASE} -U ${PGUSER} -f /scripts/03/03-1-geo-data.sql
 echo '03-2-back-data : '
-psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -f /scripts/03/03-2-back-data.sql
-echo '03-3-front-data : '
-psql -d ${POSTGRES_DB} -U ${POSTGRES_USER} -f /scripts/03/03-3-front-data.sql
+psql -d ${PGDATABASE} -U ${PGUSER} -f /scripts/03/03-2-back-data.sql
 
