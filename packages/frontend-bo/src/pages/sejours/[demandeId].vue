5632046e7187388f3974ce92580d7892ad603da8
benjamin <benjamin.pinard@sg.social.gouv.fr>
2024-02-21 15:33:35 +0100

    feat: création de la vue Dsfrtabs

--- packages/frontend-bo/src/pages/sejours/[demandeId].vue
+++ packages/frontend-bo/src/pages/sejours/[demandeId].vue
@@ -1,14 +1,105 @@
+<!--  Doc utilisée pour construire le dsfr tab : https://docs.vue-ds.fr/composants/DsfrTabs-->
+
 <template>
-  <pre>
-    {{ demandeStore.getById(route.params.demandeId) }}
-  </pre>
+  <div class="fr-container header">
+    <h1>Demande : {{ demande.libelle }}</h1>
+    <div class="detail-container">
+      <div v-for="detail in demandeDetails" :key="detail.name">
+        <strong>{{ detail.name }} : </strong>{{ detail.value }}
+      </div>
+    </div>
+    <DsfrTabs
+      tab-list-name="display-formulaire"
+      :tab-titles="tabTitles"
+      initial-selected-index="initialSelectedIndex"
+      @select-tab="selectTab"
+    >
+      <DsfrTabContent
+        panel-id="tab-content-0"
+        tab-id="tab-0"
+        :selected="selectedTabIndex === 0"
+        :asc="asc"
+      >
+        <pre>{{ demande }}</pre>
+      </DsfrTabContent>
+
+      <DsfrTabContent
+        panel-id="tab-content-1"
+        tab-id="tab-1"
+        :selected="selectedTabIndex === 1"
+        :asc="asc"
+      >
+        <div>EN CONSTRUCTION : Documents joints</div>
+      </DsfrTabContent>
+      <DsfrTabContent
+        panel-id="tab-content-2"
+        tab-id="tab-2"
+        :selected="selectedTabIndex === 2"
+        :asc="asc"
+      >
+        <div>EN CONSTRUCTION : historique</div>
+      </DsfrTabContent>
+    </DsfrTabs>
+  </div>
 </template>
 
 <script setup>
-definePageMeta({
-  middleware: ["is-connected"],
-});
+import { DsfrTabContent, DsfrTabs } from "@gouvminint/vue-dsfr";
+import { formatDate } from "date-fns/format";
+import { isDemande8Jours } from "~/utils/demande-sejour";
 
 const route = useRoute();
 const demandeStore = useDemandeSejourStore();
+
+const demande = demandeStore.getById(route.params.idDemande);
+const demandeDetails = computed(() => [
+  {
+    name: "Organisme",
+    value: demandeStore.organismeTitle(demande.demandeSejourId),
+  },
+  {
+    name: "Date (debut / fin)",
+    value: `${formatDate(demande.dateDebut, "dd/MM/yyyy")} - ${formatDate(demande.dateFin, "dd/MM/yyyy")}`,
+  },
+  { name: "Saison", value: demandeStore.saison(demande.demandeSejourId) },
+  {
+    name: "Déclaration",
+    value: isDemande8Jours(demande.statut)
+      ? "Demande à 8 jours"
+      : "Demande à 2 mois",
+  },
+  {
+    name: "Statut",
+    value: demande.statut,
+  },
+]);
+
+const tabTitles = [
+  { title: " Formulaire" },
+  { title: "Documents joints" },
+  { title: "historique" },
+];
+
+const initialSelectedIndex = 0;
+
+const asc = ref(true);
+const selectedTabIndex = ref(initialSelectedIndex);
+
+const selectTab = (idx) => {
+  asc.value = selectedTabIndex.value < idx;
+  selectedTabIndex.value = idx;
+};
 </script>
+
+<style scoped>
+.header {
+  padding: 1em 0em;
+}
+
+.detail-container {
+  display: flex;
+  flex-direction: column;
+  padding: 0em 1em;
+  margin-bottom: 2em;
+}
+</style>
---
+++ packages/frontend-bo/src/utils/demande-sejour.js
@@ -0,0 +1,21 @@
+/*  Sur l'espace admin, le status BROUILLON ne sera jamais vu,
+il ne fait donc pas parti de la liste des status possibles*/
+export const demandeSejourStatut = {
+  TRANSMISE: "TRANSMISE",
+  EN_COURS: "EN COURS",
+  A_MODIFIER: "A MODIFIER",
+  ATTENTE_HEBERGEMENT: "EN ATTENTE VALIDATION HEBERGEMENT",
+  ATTENTE_8_JOUR: "EN ATTENTE DECLARATION 8 JOURS",
+  TRANSMISE_8J: "TRANSMISE 8J",
+  VALIDEE: "VALIDEE",
+  REFUSEE: "REFUSEE",
+  MAJ_POST_8J: "MAJ POST 8J",
+};
+
+export const isDemande8Jours = (statut) =>
+  [
+    demandeSejourStatut.ATTENTE_8_JOUR,
+    demandeSejourStatut.TRANSMISE_8J,
+    demandeSejourStatut.VALIDEE,
+    demandeSejourStatut.REFUSEE,
+  ].includes(statut);
