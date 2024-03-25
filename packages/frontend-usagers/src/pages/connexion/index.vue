9e4b1e9d72503b9bf3f0ff01922a778e8ed56969
vincent-carcel <vincent.carcel@sg.social.gouv.fr>
2024-03-25 16:57:40 +0100

    feat: modif page connexion - 141

--- packages/frontend-usagers/src/pages/connexion/index.vue
+++ packages/frontend-usagers/src/pages/connexion/index.vue
@@ -1,59 +1,117 @@
 <template>
-  <div class="fr-container">
+  <div class="fr-container fr-container--fluid fr-mb-md-14v">
     <DsfrBreadcrumb :links="links" />
-    <h1>Connexion au compte</h1>
-    <div v-if="displayType" class="fr-grid-row">
-      <DsfrAlert
-        class="fr-my-3v"
-        :title="displayInfos[displayType].title"
-        :description="displayInfos[displayType].description"
-        :type="displayInfos[displayType].type"
-        :closeable="false"
-      />
-    </div>
-    <div class="fr-grid-row fr-grid-row--gutters fr-mb-1w">
-      <form class="fr-col-6">
-        <h2 class="form-title">Se connecter</h2>
-        <p>Tous les champs sont obligatoires</p>
-        <DsfrFieldset>
-          <DsfrInputGroup
-            autocomplete="off"
-            :required="true"
-            :model-value="email"
-            type="text"
-            name="email"
-            label="Adresse électronique"
-            :label-visible="true"
-            hint="Veuillez saisir votre email"
-            @update:model-value="editMail"
-          />
-          <PasswordInput
-            id="password"
-            autocomplete="off"
-            :required="true"
-            :model-value="password"
-            :type="showPassword ? 'text' : 'password'"
-            label="Mot de passe"
-            name="password"
-            hint="Veuillez saisir votre mot de passe"
-            @update:model-value="editPwd"
-          ></PasswordInput>
-          <p>
-            <NuxtLink class="fr-link" to="/connexion/mot-de-passe-oublie">
-              Mot de passe oublié ?
-            </NuxtLink>
-          </p>
-        </DsfrFieldset>
-        <DsfrButton :disabled="!canLogin" @click.prevent="login"
-          >Se connecter</DsfrButton
+    <div class="fr-grid-row fr-grid-row-gutters fr-grid-row--center">
+      <div class="fr-col-12 fr-col-md-8 fr-col-lg-6">
+        <div
+          class="fr-container fr-px-md-0 fr-py-10v fr-py-md-14v"
+          style="background-color: #f6f6f6"
         >
-      </form>
-      <div class="fr-col-6">
-        <DsfrCallout
-          title="Créer un compte"
-          content="Vous êtes responsable dans un organisme ?"
-          :button="buttonAttrs"
-        />
+          <div class="fr-grid-row fr-grid-row-gutters fr-grid-row--center">
+            <div class="fr-col-12 fr-col-md-9 fr-col-lg-8">
+              <h1>Connexion à VAO</h1>
+              <div>
+                <form id="login-1760">
+                  <fieldset
+                    id="login-1760-fieldset"
+                    class="fr-fieldset"
+                    aria-labelledby="login-1760-fieldset-legend login-1760-fieldset-messages"
+                  >
+                    <legend
+                      id="login-1760-fieldset-legend"
+                      class="fr-fieldset__legend"
+                    >
+                      <h4>Se connecter avec son compte</h4>
+                    </legend>
+
+                    <div v-if="displayType" class="fr-grid-row">
+                      <DsfrAlert
+                        class="fr-my-3v"
+                        :title="displayInfos[displayType].title"
+                        :description="displayInfos[displayType].description"
+                        :type="displayInfos[displayType].type"
+                        :closeable="false"
+                      />
+                    </div>
+                    <div class="fr-fieldset__element">
+                      <fieldset
+                        id="credentials"
+                        class="fr-fieldset"
+                        aria-labelledby="credentials-messages"
+                      >
+                        <div class="fr-fieldset__element">
+                          <span class="fr-hint-text"
+                            >Sauf mention contraire, tous les champs sont
+                            obligatoires.</span
+                          >
+                        </div>
+                        <div class="fr-fieldset__element">
+                          <div class="fr-input-group">
+                            <DsfrInput
+                              autocomplete="off"
+                              :model-value="email"
+                              type="text"
+                              name="email"
+                              label="Identifiant"
+                              :label-visible="true"
+                              hint="Format attendu : nom@domaine.fr"
+                              @update:model-value="editMail"
+                            />
+                          </div>
+                        </div>
+                        <div class="fr-fieldset__element">
+                          <div id="password-1758" class="fr-password">
+                            <div class="fr-input-wrap">
+                              <PasswordInput
+                                id="password"
+                                autocomplete="off"
+                                :model-value="password"
+                                :type="showPassword ? 'text' : 'password'"
+                                label="Mot de passe"
+                                name="password"
+                                hint="Veuillez saisir votre mot de passe"
+                                @update:model-value="editPwd"
+                              ></PasswordInput>
+                            </div>
+                            <p>
+                              <NuxtLink
+                                class="fr-link"
+                                to="/connexion/mot-de-passe-oublie"
+                              >
+                                Mot de passe oublié ?
+                              </NuxtLink>
+                            </p>
+                          </div>
+                        </div>
+                      </fieldset>
+                    </div>
+                    <div class="fr-fieldset__element">
+                      <ul class="fr-btns-group">
+                        <li>
+                          <DsfrButton
+                            :disabled="!canLogin"
+                            @click.prevent="login"
+                            >Se connecter</DsfrButton
+                          >
+                        </li>
+                      </ul>
+                    </div>
+                  </fieldset>
+                </form>
+              </div>
+              <hr />
+              <h4>Vous n’avez pas de compte ?</h4>
+              <ul class="fr-btns-group">
+                <li>
+                  <DsfrButton
+                    @click.prevent="navigateTo('/connexion/enregistrement')"
+                    >Créer un compte</DsfrButton
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -74,13 +132,10 @@ const links = [
     text: "Accueil",
   },
   {
-    to: "/connexion",
     text: "Connexion",
   },
-  {
-    text: "Identification",
-  },
 ];
+
 useHead({
   title: "VAO - connexion",
   meta: [
@@ -129,13 +184,6 @@ const displayInfos = {
   },
 };
 const displayType = ref(null);
-
-const buttonAttrs = {
-  label: "Créer un compte",
-  onClick: () => {
-    return navigateTo("/connexion/enregistrement");
-  },
-};
 
 const canLogin = computed(() => {
   return (
