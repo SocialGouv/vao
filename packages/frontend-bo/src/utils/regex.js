4191ee175b6614a4030e793ff2c675789c7dc651
l-scherer <laurent.scherer@sg.social.gouv.fr>
2024-03-26 19:21:39 +0100

    feat(frontend-bo): création nouvel utilisateur

---
+++ packages/frontend-bo/src/utils/regex.js
@@ -0,0 +1,33 @@
+const numTelephoneRegex = /^(\+33|0|0033)[1-9][0-9]{8}$/i;
+const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const siretRegex = /^[0-9]{14}$/;
+const sirenRegex = /^[0-9]{9}$/;
+const acceptedCharsRegex =
+  /^([AÀÂBCÇDEÉÈÊËFGHIÎÏJKLMNOÔPQRSTUÙÛÜVWXYŸZÆŒ\- ']+)$/i;
+const spaceFollowingDashRegex = /( -)|(- )/i;
+const doubleSpacesRegex = / {2}/i;
+const tripleDashRegex = /-{3}/i;
+const doubleDashRegex = /-{2}/i;
+const pwdRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*)(<>,~;])(?=.{12,})/;
+const pwdRegexSpecial = /^(?=.*[!@#$%^&*)(<>,~;])/;
+const pwdRegexNumber = /^(?=.*[0-9])/;
+const pwdRegexMaj = /^(?=.*[A-Z])/;
+const pwdRegexMin = /^(?=.*[a-z])/;
+
+export default {
+  numTelephoneRegex,
+  emailRegex,
+  siretRegex,
+  sirenRegex,
+  acceptedCharsRegex,
+  spaceFollowingDashRegex,
+  doubleSpacesRegex,
+  tripleDashRegex,
+  doubleDashRegex,
+  pwdRegex,
+  pwdRegexSpecial,
+  pwdRegexNumber,
+  pwdRegexMaj,
+  pwdRegexMin,
+};
---
+++ packages/frontend-bo/src/utils/serviceCompetenceOptions.js
@@ -0,0 +1,14 @@
+export const serviceCompetenceOptions = [
+  {
+    label: "Nationale",
+    value: "NAT",
+  },
+  {
+    label: "Régionale",
+    value: "REG",
+  },
+  {
+    label: "Départementale",
+    value: "DEP",
+  },
+];
