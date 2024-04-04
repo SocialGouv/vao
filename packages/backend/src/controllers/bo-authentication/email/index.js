1a4f2a861fbf32756d6ae226989567835f0c7cf6
Guillaume WEILL <gweill@guigops.fr>
2024-04-04 13:50:22 +0200

    refactor: clean authentBO.register

--- packages/backend/src/controllers/bo-authentication/email/index.js
+++ packages/backend/src/controllers/bo-authentication/email/index.js
@@ -1,6 +1,5 @@
 module.exports.forgottenPassword = require("./forgotten-password");
 module.exports.login = require("./login");
-module.exports.register = require("./register");
 module.exports.renewPassword = require("./renew-password");
 module.exports.renewToken = require("./renew-token");
 module.exports.validate = require("./validate");
