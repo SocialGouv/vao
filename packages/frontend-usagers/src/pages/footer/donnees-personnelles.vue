6731ebcc9347e5b7d8c2a53fd0a35bd93ec43206
vincent-carcel <vincent.carcel@sg.social.gouv.fr>
2024-04-04 14:03:23 +0200

    feat: mentions legales, donnees perso ok

--- packages/frontend-usagers/src/pages/footer/donnees-personnelles.vue
+++ packages/frontend-usagers/src/pages/footer/donnees-personnelles.vue
@@ -7,34 +7,214 @@
     </div>
 
     <div class="fr-grid-row">
-      <div class="fr-col">
-        <h1>Données personnelles</h1>
-      </div>
-    </div>
-    <div class="fr-grid-row">
       <div class="fr-col-12">
-        <h4>titre</h4>
-        <span>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          et bibendum lorem, a efficitur ante. Vivamus sit amet fringilla neque.
-          Vestibulum in velit egestas, tempus lectus quis, luctus mauris. Aenean
-          sit amet lacus id mauris facilisis dictum sed faucibus nulla. Donec
-          orci ex, dignissim id egestas quis, aliquet ut nunc. Nam vestibulum
-          quis lacus pellentesque ultrices. Donec mollis diam eget est hendrerit
-          viverra. Morbi eu nisl nibh. Fusce in imperdiet eros, vitae interdum
-          neque. Nam auctor dictum dui, sed ullamcorper augue mollis suscipit.
-          Fusce euismod scelerisque blandit. Fusce pretium, dolor porttitor
-          mattis venenatis, ex velit placerat nunc, non pharetra justo turpis
-          non ex. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Donec lacus lorem, tincidunt ut velit
-          et, porttitor iaculis erat. Phasellus posuere auctor velit non
-          dapibus. Sed a neque et dui pulvinar cursus. Donec vitae mi ac orci
-          ultrices molestie. Vivamus at urna et sapien feugiat dictum.
-          Vestibulum vehicula mi dui, ut egestas odio egestas et. Aenean viverra
-          scelerisque justo, at mattis ex eleifend a. Morbi ultrices mauris
-          erat. Vestibulum eu dapibus felis, ac lacinia sem. Sed non erat
-          laoreet, rhoncus tortor a, auctor metus.
-        </span>
+        <h4>Responsable du traitement</h4>
+        <p>
+          La Direction générale de la cohésion sociale (DGCS), au sein du
+          Ministère du travail, de la santé et des solidarités est responsable
+          du traitement des données personnelles pour les sites
+          vao.social.gouv.fr et bo-vao.social.gouv.fr.
+        </p>
+        <p>
+          La DGCS s’engage à ce que les traitements de données personnelles
+          effectuées sur vao.social.gouv.fr et sur bo-vao.social.gouv.fr soient
+          conformes au règlement général sur la protection des données (RGPD) et
+          à la loi Informatique et Libertés.
+        </p>
+        <p>
+          Dans le cadre de la mise à disposition de téléservices, la DGCS
+          transmet les informations collectées aux administrations partenaires
+          compétentes pour instruire les démarches accomplies. Les services des
+          conseils départementaux, des services déconcentrés des affaires
+          sociales, les services de la Direction générale de la cohésion sociale
+          et du ministère de la justice sont seuls destinataires de ces données.
+          La DGCS veille à ce que seules les personnes habilitées et autorisées
+          puissent avoir accès à ces données.
+        </p>
+        <p>
+          Les données peuvent dans une certaine mesure être rendues accessibles
+          aux prestataires (sous-traitants au sens de la règlementation) de la
+          DGCS sous son contrôle, pour les stricts besoins et dans les limites
+          de leurs missions. Ces sous-traitants sont soumis à une obligation de
+          confidentialité et ne peuvent utiliser les données à caractère
+          personnel que conformément à la législation applicable et aux
+          dispositions contractuelles spécifiquement conclues.
+        </p>
+        <p>
+          En tant que responsable de traitement, la DGCS s'engage à prendre
+          toutes les mesures nécessaires permettant de garantir la sécurité et
+          la confidentialité des informations fournies par l'usager. Elle
+          s'engage à n'opérer aucune commercialisation des informations et
+          documents transmis par l'usager au moyen des sites vao.social.gouv.fr
+          et bo-vao.social.gouv.fr, et à ne pas les communiquer à des tiers, en
+          dehors des cas prévus par la loi, notamment, en cas de réquisition
+          judiciaire.
+        </p>
+      </div>
+      <div class="fr-col-12">
+        <h4>Finalités</h4>
+        <p>
+          La DGCS met en œuvre des traitements de données à caractère personnel
+          sur la base des missions d’intérêt public dont elle est investie, pour
+          les finalités suivantes :
+        </p>
+        <ul>
+          <li>la création et la gestion d’un compte personnel ;</li>
+          <li>
+            la simplification des démarches et formalités administratives en
+            mettant à leur disposition un téléservice et en assurer la
+            traçabilité et le suivi ;
+          </li>
+        </ul>
+        <p>
+          Les données traitées par la plate-forme vao.social.gouv.fr ont
+          plusieurs fondements juridiques :
+        </p>
+        <ul>
+          <li>
+            L’obligation légale à laquelle est soumise le responsable de
+            traitements au sens de l’article 6-c du RGPD ;
+          </li>
+          <li>
+            L’exécution d’une mission d’intérêt public ou relevant de l’exercice
+            de l’autorité publique dont est investi le responsable de traitement
+            au sens de l’article 6-e du RPGD.
+          </li>
+        </ul>
+        <p>
+          Ce traitement est nécessaire à l’exécution d’une mission d’intérêt
+          public ou relevant de l’exercice de l’autorité publique dont est
+          investi le responsable de traitement au sens de l’article 6-e du
+          règlement (UE) 2016/679 du Parlement européen et du Conseil du 27
+          avril 2016 relatif à la protection des personnes physiques à l’égard
+          du traitement des données à caractère personnel et à la libre
+          circulation de ces données.
+        </p>
+      </div>
+      <div class="fr-col-12">
+        <h4>Données personnelles traitées</h4>
+        <p>
+          Les catégories de données susceptibles d'être traitées en fonction de
+          la finalité poursuivie sont les suivantes :
+        </p>
+        <ul>
+          <li>Données relatives à l'identification de la personne ;</li>
+          <li>
+            Données nécessaires à l'accomplissement de la formalité
+            administrative de demande d’une attestation d’honorabilité ;
+          </li>
+          <li>Données relatives à la mesure d'audience du site.</li>
+        </ul>
+      </div>
+      <div class="fr-col-12">
+        <h4>Durée de conservation</h4>
+        <p>
+          La DGCS s'engage à ce que vos données soient conservées pendant une
+          durée qui n'excède pas celle nécessaire au regard des finalités pour
+          lesquelles elles sont traitées.
+        </p>
+        <p>
+          Les données nécessaires à la production de statistiques d'audience et
+          d'utilisation des services en ligne sont conservées durant 25 mois
+          dans un format ne permettant pas l'identification des personnes par
+          leur adresse IP, et comportent un identifiant (relatif au cookie)
+          conservé pour une durée maximale de 6 mois sauf opposition de la
+          personne concernée.
+        </p>
+      </div>
+      <div class="fr-col-12">
+        <h4>Les Cookies</h4>
+        <p>
+          Ce site dépose un petit fichier texte (un « cookie ») sur votre
+          ordinateur lorsque vous le consultez. Cela nous permet de mesurer le
+          nombre de visites et de comprendre quelles sont les pages les plus
+          consultées. Ce site n’affiche pas de bannière de consentement aux
+          cookies, pourquoi ? C’est vrai, vous n’avez pas eu à cliquer sur un
+          bloc qui recouvre une partie de la page pour dire que vous êtes
+          d’accord avec le dépôt de cookies. Rien d’exceptionnel, nous
+          respectons simplement la loi, qui dit que certains outils de suivi
+          d’audience, correctement configurés pour respecter la vie privée, sont
+          exemptés d’autorisation préalable. Nous utilisons pour cela Matomo, un
+          outil libre, paramétré pour être en conformité avec la recommandation
+          « Cookies » de la CNIL. Cela signifie que votre adresse IP, par
+          exemple, est anonymisée avant d’être enregistrée. Il est donc
+          impossible d’associer vos visites sur ce site à votre personne.
+        </p>
+        <p>
+          En application de l’article 5(3) de la directive 2002/58/CE modifiée
+          concernant le traitement des données à caractère personnel et la
+          protection de la vie privée dans le secteur des communications
+          électroniques, transposée à l’article 82 de la loi n°78-17 du 6
+          janvier 1978 relative à l’informatique, aux fichiers et aux libertés,
+          les traceurs ou cookies suivent deux régimes distincts. Les cookies
+          strictement nécessaires au service, ceux de publicité non
+          personnalisée ou n’ayant pas pour finalité exclusive de faciliter la
+          communication par voie électronique sont dispensés de consentement
+          préalable au titre de l’article 82 de la loi n°78-17 du 6 janvier
+          1978. Les autres cookies n’étant pas strictement nécessaires au
+          service ou n’ayant pas pour finalité exclusive de faciliter la
+          communication par voie électronique doivent être consenti par
+          l’utilisateur.
+        </p>
+      </div>
+      <div class="fr-col-12">
+        <h4>Sécurité et confidentialité</h4>
+        <p>
+          Les mesures de sécurité sont mises en œuvre conformément à la
+          politique de sécurité des systèmes d’information (PSSI) des ministères
+          chargés des affaires sociales (MCAS), issue de la PSSI de l’Etat.
+        </p>
+      </div>
+      <div class="fr-col-12">
+        <h4>Droits des personnes concernées</h4>
+        <p>
+          Vous disposez des droits suivants concernant vos données à caractère
+          personnel, d’un droit de rectification et le cas échéant de
+          suppression des données. Vous pouvez accéder et obtenir copie des
+          données vous concernant, vous opposer au traitement de ces données,
+          les faire rectifier si elles sont inexactes ou les faire effacer dans
+          certains cas précis. Vous disposez également d'un droit à la
+          limitation du traitement de vos données. Le droit à l’effacement est
+          non applicable dans la mesure où le traitement est fondé sur une
+          obligation légale .Vous pouvez exercer vos droits ou obtenir des
+          informations concernant le traitement de vos données personnelles
+          directement auprès du responsable de traitement par courriel à
+          l'adresse suivante :
+        </p>
+        <p>
+          Si la réponse apportée ne vous convient pas, vous pouvez saisir le
+          délégué à la protection des données:
+        </p>
+        <ul>
+          <li>
+            par courriel à l'adresse suivante: dpd-minsociaux@sg.social.gouv.fr
+            ;
+          </li>
+          <li>par voie postale à l'adresse ci dessous.</li>
+        </ul>
+        <em
+          ><dl>
+            <dt>Direction du Numérique des ministères sociaux</dt>
+            <dt>Ministère des solidarités et de la santé</dt>
+            <dt>39-43 Quai André Citroën</dt>
+            <dt>75015 PARIS</dt>
+          </dl>
+        </em>
+        <p>
+          Nous vous invitons à joindre à votre demande toute pièce permettant de
+          justifier votre identité et le bien-fondé de votre demande. En tout
+          état de cause, l'usager dispose également du droit d'introduire une
+          réclamation ou une plainte auprès de la Commission Nationale de
+          l'Informatique et des Libertés via l'adresse
+        </p>
+
+        <a
+          title="formulaire de plainte de la CNIL"
+          href="https://www.cnil.fr/fr/plaintes"
+          target="_blank"
+          rel="noopener external"
+          >Réclamation, plainte auprès de la CNIL</a
+        >
       </div>
     </div>
   </div>
