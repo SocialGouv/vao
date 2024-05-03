e73e5339268de24ead7c5b69b2d31364d9ff5d7f
Guillaume WEILL <gweill@guigops.fr>
2024-05-03 18:55:54 +0200

    fix(organisme): accéder à la page déclaration avec un organisme incomplet fait tomber le serveur

--- packages/backend/src/controllers/organisme/getMine.js
+++ packages/backend/src/controllers/organisme/getMine.js
@@ -9,7 +9,7 @@ module.exports = async function get(req, res, next) {
   const { decoded } = req;
   const { id: userId } = decoded;
   try {
-    const organisme = await Organisme.get({
+    const organisme = await Organisme.getOne({
       use_id: userId,
     });
     return res.status(200).json({ organisme });
