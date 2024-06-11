1fafc761ee39e3c1ffd63d13964c0ce5328ef3f1
vincent-carcel <vincent.carcel@sg.social.gouv.fr>
2024-06-11 17:29:45 +0200

    fix: modifier la mise en page de l'ajout de personnel - 414

--- packages/frontend-usagers/src/components/DS/personnel.vue
+++ packages/frontend-usagers/src/components/DS/personnel.vue
@@ -119,62 +119,82 @@
       @close="onClose"
     >
       <div v-if="props.modifiable" class="fr-fieldset__element">
+        <span class="fr-label">1. Ajouter le personnel manuellement</span>
+        <span class="fr-hint-text"
+          >Cliquer sur le bouton et renseigner les champs un par un. Faire ceci
+          autant de fois que nécessaire</span
+        >
+      </div>
+      <div v-if="props.modifiable" class="fr-fieldset__element">
+        <DsfrButton
+          ref="modalOrigin"
+          :label="props.labelBoutonAjouter"
+          size="sm"
+          :secondary="true"
+          @click.prevent="addPersonne"
+        />
+      </div>
+      <div v-if="props.modifiable" class="fr-fieldset__element">
         <DsfrInputGroup
           name="pasteFrom"
           :model-value="pasteFrom"
-          label="Collez des données depuis un tableur"
-          hint="copier les cellules depuis votre tableur en respectant bien l'ordre des colonnes suivants : nom ; prénom ; date de naissance ; compétences ; fonctions ; numéro téléphone"
+          label="2. Coller des données depuis un tableur"
+          hint="Coller les cellules copiées directement depuis votre tableur (Excel, LibreOfficeCalc ...) en respectant bien l'ordre des colonnes suivants : nom ; prénom ; date de naissance ; compétences ; fonctions ; numéro téléphone"
           :label-visible="true"
           :is-textarea="true"
           placeholder="nom;prénom;date de naissance;compétences;fonctions (séparées par des virgules s'il y en a plusieurs);numéro téléphone"
           @update:model-value="handlePaste"
         />
       </div>
+      <div class="fr-fieldset__element">
+        <span class="fr-label">3. Liste personnel ajouté</span>
+      </div>
     </DsfrFieldset>
-    <div class="fr-mb-5w">
-      <!-- Cette div sert a compenser le margin bottom par défault des dsfr-table qui est de 2.5rem.
+    <DsfrFieldset>
+        <!-- Cette div sert a compenser le margin bottom par défault des dsfr-table qui est de 2.5rem.
           On cherche a rapprocher le bouton du tableau -->
-      <div class="fr-mb-n6v">
-        <UtilsTableFull
+      <div class="fr-fieldset__element">
+          <UtilsTableFull
           :headers="headers"
           :data="props.personnes"
           @click-row="editItem"
         />
       </div>
-      <DsfrButton
-        v-if="props.modifiable"
-        ref="modalOrigin"
-        :label="props.labelBoutonAjouter"
-        size="sm"
-        :secondary="true"
-        @click.prevent="addPersonne"
-      />
+      <div class="fr-fieldset__element">
+        <DsfrButton
+          ref="modalOrigin"
+          label="Export CSV"
+          size="sm"
+          :secondary="true"
+          @click.prevent="exportCSV"
+        />
+      </div>
+    </DsfrFieldset>
 
-      <DsfrModal
-        ref="modal"
-        name="test"
-        :opened="modalPersonne.opened"
-        :title="props.titre"
-        size="md"
-        @close="onClose"
-      >
-        <Personne
-          :modifiable="props.modifiable"
-          :personne="personnel"
-          :show-adresse="props.showAdresse"
-          :show-attestation="props.showAttestation"
-          :show-competence="props.showCompetence"
-          :show-date-naissance="props.showDateNaissance"
-          :show-email="props.showEmail"
-          :show-fonction="props.showFonction"
-          :show-liste-fonction="props.showListeFonction"
-          :show-telephone="props.showTelephone"
-          :show-button="props.modifiable"
-          :validate-on-mount="true"
-          @valid="updatePersonne"
-        ></Personne>
-      </DsfrModal>
-    </div>
+    <DsfrModal
+      ref="modal"
+      name="test"
+      :opened="modalPersonne.opened"
+      :title="props.titre"
+      size="md"
+      @close="onClose"
+    >
+      <Personne
+        :modifiable="props.modifiable"
+        :personne="personnel"
+        :show-adresse="props.showAdresse"
+        :show-attestation="props.showAttestation"
+        :show-competence="props.showCompetence"
+        :show-date-naissance="props.showDateNaissance"
+        :show-email="props.showEmail"
+        :show-fonction="props.showFonction"
+        :show-liste-fonction="props.showListeFonction"
+        :show-telephone="props.showTelephone"
+        :show-button="props.modifiable"
+        :validate-on-mount="true"
+        @valid="updatePersonne"
+      ></Personne>
+    </DsfrModal>
   </div>
 </template>
 
@@ -276,22 +296,6 @@ const headers = [
     column: "telephone",
     sorter: "telephone",
     text: "Téléphone",
-    headerAttrs: {
-      class: "suivi",
-    },
-  },
-  {
-    column: "erreurs",
-    sorter: "erreurs",
-    text: "Statut personne",
-    format: (value) => {
-      return {
-        component: DsfrBadge,
-        label: value.erreurs?.length > 0 ? "Incomplet" : "Complet",
-        noIcon: true,
-        type: value.erreurs?.length > 0 ? "warning" : "success",
-      };
-    },
     headerAttrs: {
       class: "suivi",
     },
@@ -441,6 +445,23 @@ async function handlePaste(lignesCollees) {
     });
   }
   emit("updatePersonne", encadrantsFromCsv);
+}
+
+function exportCSV() {
+  const rows = [
+    "Nom;Prenom;Date de Naissance;Compétences;Fonctions;Téléphone\n",
+  ];
+  props.personnes.forEach((p) => {
+    rows.push(
+      `${p.nom};${p.prenom};${dayjs(p.dateNaissance).format("DD/MM/YYYY")};${p.competence};${p.listeFonction.join(",")};${p.telephone}\n`,
+    );
+  });
+  const blob = new Blob(rows, { type: "text/csv;charset-utf-8" });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", "personnel.csv");
+  link.click();
 }
 
 function exportCSV() {
