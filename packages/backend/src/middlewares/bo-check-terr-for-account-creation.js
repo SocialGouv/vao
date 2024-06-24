e4cd7f86576e01ad8475a9c1647dde300b6f37b6
benjamin <benjamin.pinard@sg.social.gouv.fr>
2024-06-24 15:48:15 +0200

    feat(BO): visibilite des comptes

---
+++ packages/backend/src/middlewares/bo-check-terr-for-account-creation.js
@@ -0,0 +1,34 @@
+const logger = require("../utils/logger");
+const AppError = require("../utils/error");
+
+const log = logger(module.filename);
+
+function checkTerrForAccountCreation(req, res, next) {
+  const { territoireCode } = req.decoded;
+  const { departements } = req;
+  const userToCreateTerrCode = req.body.territoireCode;
+
+  log.i("IN", { territoireCode });
+
+  const canCreate =
+    territoireCode === "FRA" ||
+    (territoireCode && territoireCode.toString() === userToCreateTerrCode) ||
+    (departements &&
+      Array.isArray(departements) &&
+      departements.map((d) => d.value).includes(userToCreateTerrCode));
+
+  if (!canCreate) {
+    return next(
+      new AppError(
+        "L'administrateur ne peut pas créer de comptes en dehors de son territoire",
+        {
+          statusCode: 403,
+        },
+      ),
+    );
+  }
+  log.i("DONE");
+  next();
+}
+
+module.exports = checkTerrForAccountCreation;
