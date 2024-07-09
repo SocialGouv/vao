f8b8c416ee268d4a5d463f274cbb89d0aba91dca
iNeoO <valere.pique@sg.social.gouv.fr>
2024-07-09 15:16:41 +0200

    feat(CardsNumber): Create composent

---
+++ packages/frontend-bo/src/components/utils/CardsNumber.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cards-number-wrapper fr-pb-3w">
+    <card-number
+      v-for="v in values"
+      :key="v.title"
+      :title="v.title"
+      :value="v.value"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CardNumber from "~/components/utils/CardNumber.vue";
+
+defineProps<{
+  values: Array<{ title: string; value: number }>;
+}>();
+</script>
+
+<style lang="scss">
+.cards-number-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+}
+</style>
