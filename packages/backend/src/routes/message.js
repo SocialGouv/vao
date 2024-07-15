558278f3014dbd2e546a13fd6e01d8f154ce962d
vincent-carcel <vincent.carcel@sg.social.gouv.fr>
2024-07-15 11:41:09 +0200

    fix: ajout du middleware

--- packages/backend/src/routes/message.js
+++ packages/backend/src/routes/message.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const checkJWT = require("../middlewares/checkJWT");
 const boCheckJWT = require("../middlewares/bo-check-JWT");
 const boCheckRole = require("../middlewares/bo-check-role");
-
+const checkPermissionDeclarationSejour = require("../middlewares/checkPermissionDeclarationSejour");
 const messageController = require("../controllers/message");
 const getDepartements = require("../middlewares/getDepartements");
 
@@ -17,14 +17,29 @@ const boCheckRoleDS = boCheckRole([
 router.post(
   "/admin/:declarationId",
   boCheckJWT,
+
   boCheckRoleDS,
   getDepartements,
   messageController.postByBO,
 );
 
-router.post("/:declarationId", checkJWT, messageController.postByFO);
-
-router.get("/admin/:declarationId", boCheckJWT, messageController.get);
-router.get("/:declarationId", checkJWT, messageController.get);
+router.post(
+  "/:declarationId",
+  checkJWT,
+  checkPermissionDeclarationSejour,
+  messageController.postByFO,
+);
+router.get(
+  "/admin/:declarationId",
+  boCheckJWT,
+  checkPermissionDeclarationSejour,
+  messageController.get,
+);
+router.get(
+  "/:declarationId",
+  checkJWT,
+  checkPermissionDeclarationSejour,
+  messageController.get,
+);
 
 module.exports = router;
