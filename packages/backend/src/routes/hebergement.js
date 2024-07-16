2d7155b62e8dcbb53421dce72a6c5a33185d7455
vincent-carcel <vincent.carcel@sg.social.gouv.fr>
2024-07-16 16:45:00 +0200

    fix: les hebergements sont à présent relies a l'organismeId et non au userId

--- packages/backend/src/routes/hebergement.js
+++ packages/backend/src/routes/hebergement.js
@@ -4,13 +4,24 @@ const router = express.Router();
 
 const checkJWT = require("../middlewares/checkJWT");
 const boCheckJWT = require("../middlewares/bo-check-JWT");
+const checkPermissionHebergement = require("../middlewares/checkPermissionHebergement");
 const hebergementController = require("../controllers/hebergement");
 
 // Gère une connexion via mot de passe.
-router.get("/:id", checkJWT, hebergementController.getById);
+router.get(
+  "/:id",
+  checkJWT,
+  checkPermissionHebergement,
+  hebergementController.getById,
+);
 router.get("/admin/:id", boCheckJWT, hebergementController.getById);
 router.get("/", checkJWT, hebergementController.get);
 router.post("/", checkJWT, hebergementController.post);
-router.post("/:id", checkJWT, hebergementController.update);
+router.post(
+  "/:id",
+  checkJWT,
+  checkPermissionHebergement,
+  hebergementController.update,
+);
 
 module.exports = router;
---
+++ packages/migrations/src/migrations/20240715182956_front__link_hebergement_on_organisme.js
@@ -0,0 +1,36 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.raw(`
+    ALTER TABLE front.hebergement add organisme_id INTEGER;
+    UPDATE front.hebergement h SET organisme_id = (
+      SELECT org_id 
+      FROM front.user_organisme uo 
+      JOIN front.users u ON u.id = uo.use_id
+      WHERE u.id = h.user_id
+      );
+    ALTER TABLE front.hebergement ADD CONSTRAINT pk_hebergement_organisme FOREIGN KEY (organisme_id) REFERENCES front.organismes(id);
+    ALTER TABLE front.hebergement DROP user_id;
+  `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.raw(`
+    ALTER TABLE front.hebergement add user_id INTEGER;
+    UPDATE front.hebergement h SET user_id = (
+      SELECT org_id 
+      FROM front.user_organisme uo 
+      JOIN front.organismes o ON o.id = uo.org_id
+      WHERE o.id = h.organisme_id
+      LIMIT 1
+      );
+    ALTER TABLE front.hebergement ADD CONSTRAINT pk_hebergement_user FOREIGN KEY (user_id) REFERENCES front.users(id);
+    ALTER TABLE front.hebergement DROP organisme_id;
+  `);
+};
