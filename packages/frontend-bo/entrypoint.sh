c19341107c17f53762b250a655eaabbdb13689c9
iNeoO <valere.pique@sg.social.gouv.fr>
2024-07-16 16:30:30 +0200

    WIP

--- packages/frontend-bo/entrypoint.sh
+++ packages/frontend-bo/entrypoint.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 
 rm -rf /app/packages/frontend-bo/node_modules
-cp -r /usr/cache/node_modules /app/packages/frontend-bo/node_modules
-cp -r /usr/cache/package.json /app/packages/frontend-bo/package.json
-cp -r /usr/cache/yarn.lock /app/packages/frontend-bo/yarn.lock
+cp -r /usr/cache/frontend-bo/node_modules /app/packages/frontend-bo/node_modules
+cp -r /usr/cache/frontend-bo/package*.json /app/packages/frontend-bo/package*.json
+cp -r /usr/cache/frontend-bo/yarn.lock /app/packages/frontend-bo/yarn.lock
 exec yarn workspace @vao/frontend-bo dev
--- packages/frontend-usagers/entrypoint.sh
+++ packages/frontend-usagers/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf /app/packages/frontend-usagers/node_modules
-cp -r /usr/cache/node_modules /app/packages/frontend-usagers/node_modules
-cp -r /usr/cache/package.json /app/packages/frontend-usagers/package.json
-cp -r /usr/cache/yarn.lock /app/packages/frontend-usagers/yarn.lock
+cp -r /usr/cache/frontend-usagers/node_modules /app/packages/frontend-usagers/node_modules
+cp -r /usr/cache/frontend-usagers/package*.json /app/packages/frontend-usagers/package*.json
+cp -r /usr/cache/frontend-usagers/yarn.lock /app/packages/frontend-usagers/yarn.lock
 exec yarn workspace @vao/frontend-usagers dev
