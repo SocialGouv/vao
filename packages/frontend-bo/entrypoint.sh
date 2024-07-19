87edd88267aa86619a50676eec89d6c012e74f9d
iNeoO <valere.pique@sg.social.gouv.fr>
2024-07-19 10:38:19 +0200

    feat: try to fix ci

--- packages/frontend-bo/entrypoint.sh
+++ packages/frontend-bo/entrypoint.sh
@@ -1,6 +1,8 @@
 
 rm -rf /app/packages/frontend-bo/node_modules
+cp -r /usr/cache/node_modules /app/node_modules
 cp -r /usr/cache/packages/frontend-bo/node_modules /app/packages/frontend-bo/node_modules
 cp -r /usr/cache/packages/frontend-bo/package*.json /app/packages/frontend-bo/package*.json
 cp -r /usr/cache/packages/frontend-bo/yarn.lock /app/packages/frontend-bo/yarn.lock
+cp -r /usr/cache/packages/frontend-bo/.nuxt /app/packages/frontend-bo/.nuxt
 exec yarn workspace @vao/frontend-bo dev
--- packages/frontend-usagers/entrypoint.sh
+++ packages/frontend-usagers/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rm -rf /app/packages/frontend-usagers/node_modules
+cp -r /usr/cache/node_modules /app/node_modules
 cp -r /usr/cache/packages/frontend-usagers/node_modules /app/packages/frontend-usagers/node_modules
 cp -r /usr/cache/packages/frontend-usagers/package*.json /app/packages/frontend-usagers/package*.json
 cp -r /usr/cache/packages/frontend-usagers/yarn.lock /app/packages/frontend-usagers/yarn.lock
+cp -r /usr/cache/packages/frontend-usagers/.nuxt /app/packages/frontend-usagers/.nuxt
 exec yarn workspace @vao/frontend-usagers dev
