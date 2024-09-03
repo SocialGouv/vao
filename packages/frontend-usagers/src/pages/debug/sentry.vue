61741115b9c8ae733623aa54ed1a8e383bccf399
Adrien Chauve <adrien@chauve.eu>
2024-09-03 09:35:12 +0200

    chore: use runtime-compatible nginx based config

---
+++ packages/frontend-usagers/src/pages/debug/sentry.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fr-container fr-container--fluid fr-my-md-14v">
+    <button @click="testSentrySourceMaps">Test Sentry Source Maps</button>
+  </div>
+</template>
+
+<script setup>
+function testSentrySourceMaps() {
+  const msg = "testing sentry source maps";
+  console.log(msg);
+  doTestSentry();
+}
+
+function doTestSentry() {
+  throw new Error("Test sentry sourcemaps");
+}
+</script>
+
+<style lang="scss" scoped></style>
