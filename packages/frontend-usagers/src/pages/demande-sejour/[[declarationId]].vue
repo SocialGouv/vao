898d3a1bf5258329e42939526c4676c6d1552a63
benjamin <benjamin.pinard@sg.social.gouv.fr>
2024-09-17 12:17:56 +0200

    fix: after rebase +some bug

--- packages/frontend-usagers/src/pages/demande-sejour/[[declarationId]].vue
+++ packages/frontend-usagers/src/pages/demande-sejour/[[declarationId]].vue
@@ -692,7 +692,7 @@ function nextHash() {
   return navigateTo({
     path: `/demande-sejour/${sejourId.value}`,
     hash: "#" + sommaireOptions[index + 1],
-  "#synthese": () => titleStart.value + "étape 8 sur 8 | Synthèse" + titleEnd,
+    "#synthese": () => titleStart.value + "étape 8 sur 8 | Synthèse" + titleEnd,
   });
 }
 
