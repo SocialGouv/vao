e9c375e8b05ce63d09477dd8e5b52e8de47d39a6
Adrien Chauve <adrien@chauve.eu>
2024-09-25 14:36:37 +0200

    fix: make a simpler SQL request

--- packages/backend/src/services/Healthz.js
+++ packages/backend/src/services/Healthz.js
@@ -2,7 +2,7 @@ const pool = require("../utils/pgpool").getPool();
 
 const query = {
   // simple query to check if the connexion to the db is up
-  healthz: () => [`SELECT rol_id FROM back.user_roles LIMIT 1;`, []],
+  healthz: () => [`SELECT NOW();`, []],
 };
 
 module.exports.healthz = async () => {
