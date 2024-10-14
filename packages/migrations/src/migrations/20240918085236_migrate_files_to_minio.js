b648a2ff61651d11a1d9d628121eba793f81b8c6
Adrien Chauve <adrien@chauve.eu>
2024-10-14 12:06:53 +0200

    fix: disable s3 migration

--- packages/migrations/src/migrations/20240918085236_migrate_files_to_minio.js
+++ packages/migrations/src/migrations/20240918085236_migrate_files_to_minio.js
@@ -22,65 +22,63 @@ const s3Client = new S3Client({
  * @returns { Promise<void> }
  */
 exports.up = (knex) => {
-  console.log("Migrating files to S3");
-  console.log("S3_BUCKET_NAME", S3_BUCKET_NAME);
-  console.log("S3_BUCKET_ROOT_DIR", S3_BUCKET_ROOT_DIR);
-  return knex
-    .withSchema("doc")
-    .select()
-    .from("documents")
-    .then(async (rows) => {
-      for (const row of rows) {
-        const objectKey = `${S3_BUCKET_ROOT_DIR}/${row.uuid}.pdf`;
-
-        try {
-          // Check if the file already exists
-          await s3Client.send(
-            new HeadObjectCommand({
-              Bucket: S3_BUCKET_NAME,
-              Key: objectKey,
-            }),
-          );
-
-          // If the file exists, log it and skip the upload
-          console.log(`File ${objectKey} already exists. Skipping upload.`);
-          continue;
-        } catch (err) {
-          // If a 404 error occurs, the file doesn't exist, proceed with upload
-          if (err.name === "NotFound") {
-            try {
-              // Upload the file since it doesn't exist
-              await s3Client.send(
-                new PutObjectCommand({
-                  Body: row.file,
-                  Bucket: S3_BUCKET_NAME,
-                  Key: objectKey,
-                  Metadata: {
-                    category: String(row.category),
-                    created_at: String(row.created_at),
-                    mimetype: String(row.mime_type),
-                    originalname: String(row.filename),
-                  },
-                }),
-              );
-              console.log(`Uploaded ${objectKey}`);
-            } catch (uploadErr) {
-              console.error(`Failed to upload ${objectKey}:`, uploadErr);
-              throw uploadErr;
-            }
-          } else {
-            // If the error isn't a 404, throw it to handle other failures
-            console.error(`Error checking existence of ${objectKey}:`, err);
-            throw err;
-          }
-        }
-      }
-    })
-    .catch((err) => {
-      // Catch the error and rethrow it to ensure the migration fails
-      console.error("Migration failed:", err);
-      throw err;
-    });
+  // console.log("Migrating files to S3");
+  // console.log("S3_BUCKET_NAME", S3_BUCKET_NAME);
+  // console.log("S3_BUCKET_ROOT_DIR", S3_BUCKET_ROOT_DIR);
+  // return knex
+  //   .withSchema("doc")
+  //   .select()
+  //   .from("documents")
+  //   .then(async (rows) => {
+  //     for (const row of rows) {
+  //       const objectKey = `${S3_BUCKET_ROOT_DIR}/${row.uuid}.pdf`;
+  //       try {
+  //         // Check if the file already exists
+  //         await s3Client.send(
+  //           new HeadObjectCommand({
+  //             Bucket: S3_BUCKET_NAME,
+  //             Key: objectKey,
+  //           }),
+  //         );
+  //         // If the file exists, log it and skip the upload
+  //         console.log(`File ${objectKey} already exists. Skipping upload.`);
+  //         continue;
+  //       } catch (err) {
+  //         // If a 404 error occurs, the file doesn't exist, proceed with upload
+  //         if (err.name === "NotFound") {
+  //           try {
+  //             // Upload the file since it doesn't exist
+  //             await s3Client.send(
+  //               new PutObjectCommand({
+  //                 Body: row.file,
+  //                 Bucket: S3_BUCKET_NAME,
+  //                 Key: objectKey,
+  //                 Metadata: {
+  //                   category: String(row.category),
+  //                   created_at: String(row.created_at),
+  //                   mimetype: String(row.mime_type),
+  //                   originalname: String(row.filename),
+  //                 },
+  //               }),
+  //             );
+  //             console.log(`Uploaded ${objectKey}`);
+  //           } catch (uploadErr) {
+  //             console.error(`Failed to upload ${objectKey}:`, uploadErr);
+  //             throw uploadErr;
+  //           }
+  //         } else {
+  //           // If the error isn't a 404, throw it to handle other failures
+  //           console.error(`Error checking existence of ${objectKey}:`, err);
+  //           throw err;
+  //         }
+  //       }
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     // Catch the error and rethrow it to ensure the migration fails
+  //     console.error("Migration failed:", err);
+  //     throw err;
+  //   });
 };
 
 /**
