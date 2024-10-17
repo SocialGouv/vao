ca6a752c19cc8c910229ff43d1a8a8588b8dcc98
benjamin <benjamin.pinard@sg.social.gouv.fr>
2024-10-17 15:48:21 +0200

    feat(bo-comptes): refacto

---
+++ packages/frontend-bo/src/utils/bo-user.js
@@ -0,0 +1,40 @@
+import * as yup from "yup";
+import { emailSchema } from "@vao/shared/src/schema/email";
+import { prenomSchema } from "@vao/shared/src/schema/prenom";
+import { nomSchema } from "@vao/shared/src/schema/nom";
+import {
+  serviceCompetenceDEP,
+  serviceCompetenceNAT,
+  serviceCompetenceREG,
+} from "./serviceCompetenceOptions";
+
+const BoUserSchema = {
+  isActive: yup.boolean().required(),
+  email: emailSchema(),
+  nom: nomSchema(),
+  prenom: prenomSchema(),
+  roles: yup.array().min(1, "Il faut au moins sélectionner un role").required(),
+  serviceCompetence: yup
+    .string()
+    .oneOf([
+      serviceCompetenceNAT.value,
+      serviceCompetenceDEP.value,
+      serviceCompetenceREG.value,
+    ])
+    .required(),
+  territoireCode: yup.string().when("serviceCompetence", {
+    is: (val) => {
+      return (
+        val === serviceCompetenceDEP.value || val === serviceCompetenceREG.value
+      );
+    },
+    then: (schema) => {
+      return schema.required(
+        "Le territoire  doit obligatoirement être renseigné",
+      );
+    },
+    otherwise: (schema) => schema.nullable().strip(),
+  }),
+};
+
+export default { BoUserSchema };
