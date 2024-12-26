1783bae7135c822f2145cf687a328eb1d86b229b
Valère Pique <valere.pique@sg.social.gouv.fr>
2024-12-26 10:35:51 +0100

    feat(apiToken): generate apiToken in backend, serve it in frontend

---
+++ packages/backend/src/services/ApiToken.js
@@ -0,0 +1,36 @@
+const pool = require("../utils/pgpool").getPool();
+
+const getApiTokenQuery = `
+  SELECT
+    ak.api_token AS "apiToken",
+    ak.expires_at AS "expiresAt"
+  FROM front.api_keys ak
+  WHERE ak.user_id = $1
+`;
+
+const createOrUpdateApiTokenQuery = `
+  INSERT INTO front.api_keys (user_id, api_token, created_at, expires_at)
+  VALUES ($1, $2, NOW(), $3)
+  ON CONFLICT (user_id)
+  DO UPDATE SET
+    api_token = EXCLUDED.api_token,
+    created_at = NOW(),
+    expires_at = EXCLUDED.expires_at
+`;
+
+module.exports.getApiToken = async (userId) => {
+  const result = await pool.query(getApiTokenQuery, [userId]);
+  if (result.rows.length === 0) {
+    return { apiToken: null, expiresAt: null };
+  }
+  return result.rows[0];
+};
+
+module.exports.createOrUpdateApiToken = async ({
+  apiToken,
+  expiresAt,
+  userId,
+}) => {
+  await pool.query(createOrUpdateApiTokenQuery, [userId, apiToken, expiresAt]);
+  return { apiToken, expiresAt };
+};
