6a60e4266212cd58e306c31db4409f9347c4e9c4
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-01-21 10:57:39 +0100

    feat: add volume data for pg

--- packages/migrations/start.sh
+++ packages/migrations/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 yarn workspace @vao/migrations install
-exec tail -f /dev/null
-# exec yarn workspace @vao/migrations run knex --cwd ./src migrate:latest
+exec yarn workspace @vao/migrations run knex --cwd ./src migrate:latest
