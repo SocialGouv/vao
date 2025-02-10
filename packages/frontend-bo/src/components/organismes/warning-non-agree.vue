0df79558c1f2bedaca8240bd5e60f0298c1d3ab9
benjamin <benjamin.pinard@sg.social.gouv.fr>
2025-02-10 15:36:55 +0100

    fix(warning): display date

--- packages/frontend-bo/src/components/organismes/warning-non-agree.vue
+++ packages/frontend-bo/src/components/organismes/warning-non-agree.vue
@@ -4,13 +4,14 @@
     key="alert-non-agree"
     type="error"
     title="Organisme non agréé"
-    :description="`L’organisme ${libelle} n’est plus agréé depuis le ${dateFinValidite} ${dayjs(dateFinValidite).format('DD/MM/YYYY')} pour la raison suivante : expiration`"
+    :description="`L’organisme ${libelle} n’est plus agréé depuis le ${dayjs(dateFinValidite).locale('fr').format('DD MMMM YYYY')} pour la raison suivante : expiration`"
     title-tag="h3"
   />
 </template>
 
 <script setup>
 import dayjs from "dayjs";
+import "dayjs/locale/fr";
 
 defineProps({
   libelle: { type: String, default: "" },
