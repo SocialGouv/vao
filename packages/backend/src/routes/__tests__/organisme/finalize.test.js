1b7d5f65202efa48514c5f8fd40d126ac8a4a431
l-scherer <laurent.scherer@sg.social.gouv.fr>
2025-02-12 09:46:19 +0100

    feat/test ajout test route organisme finalize

---
+++ packages/backend/src/routes/__tests__/organisme/finalize.test.js
@@ -0,0 +1,37 @@
+const checkJWT = require("../../../middlewares/checkJWT");
+const checkPermissionOrganisme = require("../../../middlewares/checkPermissionOrganisme");
+const request = require("supertest");
+const app = require("../../../app");
+const Organisme = require("../../../services/Organisme");
+
+jest.mock("../../../middlewares/checkJWT");
+jest.mock("../../../middlewares/checkPermissionOrganisme");
+jest.mock("../../../services/Organisme");
+
+describe("POST /organisme/:id/finalize", () => {
+  const user = {
+    id: 1,
+  };
+  beforeEach(() => {
+    jest.clearAllMocks();
+    checkJWT.mockImplementation((req, res, next) => {
+      req.decoded = { ...user };
+      next();
+    });
+    checkPermissionOrganisme.mockImplementation((req, res, next) => {
+      next();
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return a 200", async () => {
+    const response = await request(app).post("/organisme/1/finalize");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("message", "sauvegarde organisme OK");
+    expect(Organisme.finalize).toHaveBeenCalled();
+  });
+
+});
