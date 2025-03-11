7567f3109c2ff7c999e8c7d550a6f74f50b9b227
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-03-11 15:01:13 +0100

    feat: add health

--- packages/external-api/src/app.module.ts
+++ packages/external-api/src/app.module.ts
@@ -14,6 +14,7 @@ import configuration from "./config/configuration";
 import { FileModule } from "./file/file.module";
 import { DatabaseModule } from "./global/database.module";
 import { S3Module } from "./global/s3.module";
+import { HealthModule } from "./health/health.module";
 import { OrganismModule } from "./organism/organism.module";
 import { UserModule } from "./user/user.module";
 
@@ -24,7 +25,6 @@ import { UserModule } from "./user/user.module";
       isGlobal: true,
       load: [configuration],
     }),
-    // TODO
     SentryModule.forRoot(),
     LoggerModule.forRootAsync({
       imports: [ConfigModule],
@@ -53,6 +53,7 @@ import { UserModule } from "./user/user.module";
           },
         };
       },
+      providers: undefined,
     }),
     DatabaseModule,
     S3Module,
@@ -61,6 +62,7 @@ import { UserModule } from "./user/user.module";
     FileModule,
     OrganismModule,
     UserModule,
+    HealthModule,
   ],
   providers: [
     {
