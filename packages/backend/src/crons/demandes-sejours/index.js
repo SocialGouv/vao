e1c472a9d21468a7cfb5fa34e8b0d292cda140b0
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-03-18 17:35:24 +0100

    feat: remove legacy cron

--- packages/backend/src/crons/demandes-sejours/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.notifyRappelActionsBO = require("./notifyRappelActionsBO").job;
-module.exports.notifyRappelds8j15j = require("./notifyRappelds8j15j").job;
-module.exports.updateStatutDS = require("./updateStatutDS").job;
