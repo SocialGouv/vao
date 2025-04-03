0b92e652998222ac57b44df80f07c8033d270187
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-04-03 15:18:39 +0200

    feat: update way users are created

--- packages/backend/src/controllers/authentication/email/login.js
+++ packages/backend/src/controllers/authentication/email/login.js
@@ -42,7 +42,16 @@ module.exports = async function login(req, res, next) {
       log.w("Compte non validé");
       return next(
         new AppError("Compte inactif", {
-          name: "NotValidatedAccount",
+          name: "NeedEmailValidation",
+          statusCode: 400,
+        }),
+      );
+    }
+    if (user.statusCode === status.NEED_SIRET_VALIDATION) {
+      log.w("Compte non validé");
+      return next(
+        new AppError("Compte inactif", {
+          name: "NeedSiretValidation",
           statusCode: 400,
         }),
       );
