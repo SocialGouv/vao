2463233e77d8e3740e80668f3ee6938d342a9bfd
l-scherer <laurent.scherer@sg.social.gouv.fr>
2025-05-27 10:46:54 +0200

    fix(bo): change route pour download doc

--- packages/frontend-bo/src/pages/sejours/[declarationId]/messagerie.vue
+++ packages/frontend-bo/src/pages/sejours/[declarationId]/messagerie.vue
@@ -2,7 +2,7 @@
   <Chat
     ref="chatRef"
     :messages="messages"
-    :cdn-url="`${config.public.backendUrl}/documents/`"
+    :cdn-url="`${config.public.backendUrl}/documents/admin`"
     :is-loading="isSendingMessage"
     @send="sendMessage"
   />
