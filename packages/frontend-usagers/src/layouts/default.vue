f312acc3bcfee853f097e86175f1ba7753bb7325
olivier-rabot <olivier.rabot@sg.social.gouv.fr>
2025-07-09 16:07:52 +0200

    feat: ajout page erreur nuxt personnalisee

--- packages/frontend-usagers/src/layouts/default.vue
+++ packages/frontend-usagers/src/layouts/default.vue
@@ -1,69 +1,14 @@
 <script setup>
-const log = logger("layouts/default");
-const userStore = useUserStore();
+import { Header, Footer, useLayoutHeader } from "@vao/shared";
 
+const userStore = useUserStore();
 const config = useRuntimeConfig();
 
-const logoText = ["Republique", "française"];
-
-const quickLinks = computed(() => {
-  return [
-    {
-      label: "Aide",
-      href: "https://vao-assistance.atlassian.net/servicedesk/customer/portals",
-      icon: "ri:question-line",
-      iconRight: false,
-      target: "_blank",
-      rel: "noopener noreferrer",
-    },
-    ...(userStore.isConnected
-      ? [
-          {
-            label: "Mon compte",
-            to: "/mon-compte",
-            icon: "ri:account-circle-line",
-            iconRight: false,
-          },
-        ]
-      : []),
-    ...(userStore.isConnected
-      ? [
-          {
-            label: "Se déconnecter",
-            onclick: logout,
-            icon: "ri:logout-box-line",
-            iconRight: false,
-            button: true,
-          },
-        ]
-      : []),
-  ];
+const { quickLinks, homeTo } = useLayoutHeader({
+  logoutUrl: "/authentication/disconnect",
+  accountPath: "/mon-compte",
+  userStore,
 });
-
-const homeTo = computed(() => {
-  return userStore.isConnected ? "/" : "/connexion/";
-});
-
-async function logout() {
-  const sub = userStore.user.sub;
-  log.i("logout - IN");
-  await $fetchBackend("/authentication/disconnect", {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ sub }),
-  })
-    .then(async () => {
-      await navigateTo("/connexion");
-      userStore.$reset();
-      log.i("logout - Done");
-    })
-    .catch(() => {
-      log.w("logout - Error");
-    });
-}
 
 const consentCookie = useCookie("VAO_consent", {
   default: () => false,
@@ -94,56 +39,22 @@ const navItems = useMenuNavItems();
         </ul>
       </nav>
     </div>
-    <DsfrHeader
-      id="header"
-      service-title="Vacances Adaptées Organisées (VAO)"
-      service-description="La plateforme de déclaration et suivi des séjours organisés pour les personnes handicapées majeures"
+    <Header
       :home-to="homeTo"
       :quick-links="quickLinks"
-      :show-search="false"
-      :logo-text="logoText"
-    >
-      <template #mainnav>
-        <DsfrNavigation
-          id="menu"
-          :nav-items="navItems"
-          style="box-shadow: inset 0 1px 0 0 #ddd"
-        />
-      </template>
-    </DsfrHeader>
+      :nav-items="navItems"
+      service-title="Vacances Adaptées Organisées"
+      service-description="La plateforme de déclaration et suivi des séjours organisés pour les personnes handicapées"
+    ></Header>
 
     <main id="content" role="main">
       <slot />
     </main>
 
-    <DsfrFooter
-      id="footer"
-      legal-link="/footer/mentions-legales"
-      personal-data-link="/footer/donnees-personnelles"
-      cookies-link="/footer/gestion-cookies"
-      a11y-compliance-link="/footer/accessibilite"
-      class="fr-mt-2w"
-      home-title="Retour à l'accueil Vacances Adaptées Organisés, République Française"
-    >
-      <template #description>
-        <p
-          v-if="config.public.environment !== 'production'"
-          class="fr-footer__content-desc"
-        >
-          Environnement : {{ config.public.environment }}
-        </p>
-        <a
-          v-if="config.public.appVersion"
-          href="https://vao-assistance.atlassian.net/wiki/spaces/IS/pages/91095041/MISE+A+JOUR+DE+LA+PLATEFORME+VAO"
-          class="fr-footer__content-desc"
-          title="assistance, nouvelle page"
-          target="_blank"
-          rel="noopener external"
-        >
-          Version : {{ config.public.appVersion }}
-        </a>
-      </template>
-    </DsfrFooter>
+    <Footer
+      :environment="config.public.environment"
+      :app-version="config.public.appVersion"
+    />
 
     <DsfrNotice
       v-if="!consentCookie"
