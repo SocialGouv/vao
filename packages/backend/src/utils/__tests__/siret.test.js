546f9e853afcfc0ea5c40e75b5e99d89a8ccec0d
l-scherer <laurent.scherer@sg.social.gouv.fr>
2025-10-07 16:16:07 +0200

    fix: 1170 tests adjustments

--- packages/backend/src/utils/__tests__/siret.test.js
+++ packages/backend/src/utils/__tests__/siret.test.js
@@ -1,18 +1,30 @@
 const { formatSiren, formatSiret } = require("../siret");
 
-describe("Siret", () => {
-  it("Devrait retourner un siret correctement formatté", () => {
+describe("formatSiret", () => {
+  it("should format SIRET if it's valid", () => {
     expect(formatSiret({ siret: "12345678901234" })).toBe("123 456 789 01234");
   });
-  it("devrait retourner l'entrée telle quelle si non numérique", () => {
+  it("should return input when it isn't valid", () => {
     expect(formatSiret({ siret: "123456789ABCDE" })).toBe("123456789ABCDE");
   });
+  it("should return input when it isn't a Siret lenght", () => {
+    expect(formatSiret({ siret: "123456789" })).toBe("123456789");
+  });
+  it("should return empty response when input is wrong object", () => {
+    expect(formatSiret({ siren: "12345678901234" })).toBe("");
+  });
 });
-describe("Siren", () => {
-  it("Devrait retourner un siren correctement formatté", () => {
+describe("formatSiren", () => {
+  it("should format SIREN if it's valid", () => {
     expect(formatSiren({ siren: "123456789" })).toBe("123 456 789");
   });
-  it("devrait retourner l'entrée telle quelle si non numérique", () => {
+  it("should return entry when it isn't valid", () => {
     expect(formatSiren({ siren: "12345ABCDE" })).toBe("12345ABCDE");
+  });
+  it("should return entry when it isn't a Siren", () => {
+    expect(formatSiren({ siren: "12345678901234" })).toBe("12345678901234");
+  });
+  it("should return empty response when input is wrong object", () => {
+    expect(formatSiren({ siret: "12345678901234" })).toBe("");
   });
 });
