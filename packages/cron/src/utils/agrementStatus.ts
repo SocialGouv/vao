41797c027b33380cf1b4f10c681f49c0970aaff7
l-scherer <laurent.scherer@sg.social.gouv.fr>
2025-10-23 18:35:00 +0200

    feat: 1098 cron agrement expiration notification

---
+++ packages/cron/src/utils/agrementStatus.ts
@@ -0,0 +1,14 @@
+// TODO A supprimer et utiliser l'énum partagée une fois la PR de ach mergée
+// Ref: packages/backend/src/dto/AgrementsDto.ts
+export enum AgrementStatut {
+  BROUILLON = "BROUILLON",
+  TRANSMIS = "TRANSMIS",
+  DEPOSE = "DEPOSE",
+  VERIF_EN_COURS = "VERIF_EN_COURS",
+  PRIS_EN_CHARGE = "PRIS_EN_CHARGE",
+  EN_COURS = "EN_COURS",
+  A_MODIFIER = "A_MODIFIER",
+  REFUSE = "REFUSE",
+  COMPLETUDE_CONFIRME = "COMPLETUDE_CONFIRME",
+  VALIDE = "VALIDE",
+}
