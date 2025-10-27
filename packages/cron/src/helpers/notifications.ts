026af342bb2e73b3af41bb4850ed48b38f9fcc28
l-scherer <laurent.scherer@sg.social.gouv.fr>
2025-10-27 14:22:02 +0100

    feat: 1098 cron corrections review

--- packages/cron/src/helpers/notifications.ts
+++ packages/cron/src/helpers/notifications.ts
@@ -1,3 +1,5 @@
 export const typeNotification = {
   inactifDeuxMois: "Compte Inactif 2 mois",
+  agrementExpiration6m: "Agrément expirant 6 mois",
+  agrementExpiration120j: "Agrément expirant 120 jours",
 };
