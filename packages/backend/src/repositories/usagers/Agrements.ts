1b4ca596a82814ab07c19433072c24902cde0eac
l-scherer <laurent.scherer@sg.social.gouv.fr>
2025-10-29 17:52:35 +0100

    feat: 1085 add backend get agrement by organismeId

---
+++ packages/backend/src/repositories/usagers/Agrements.ts
@@ -0,0 +1,27 @@
+import Logger from "../../utils/logger";
+import { getPool } from "../../utils/pgpool";
+
+const log = Logger(module.filename);
+
+const pool = getPool();
+
+export const AgrementsRepository = {
+  getByOrganismeId: async ({ organismeId }: { organismeId: number }) => {
+    log.i("getByOrganismeId - IN");
+
+    const query = () => `
+        SELECT
+            *
+        FROM
+        front.agrements agr
+        WHERE
+            agr.organimse_id = $1
+        `;
+    const response = await pool.query(query(), [organismeId]);
+    log.i("getAdminStats - DONE");
+
+    return {
+      agrement: response.rows[0],
+    };
+  },
+};
