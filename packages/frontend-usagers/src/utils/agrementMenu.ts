b87a048e368fdaf6c871103df6142d39bcb358ba
l-scherer <159151890+l-scherer@users.noreply.github.com>
2025-11-17 12:55:16 +0100

    feat: 1085 front usager etape 2 sur 4 (#1072)
    ## Ticket(s) lié(s)
    1085 (suite)
    ## Description
    Mise en place de l'étape 2/4 (dossier de candidature renouvellement
    agrément)
    get et Post depuis le Store
    Activation du hash pour navigation du menu à gauche
    Réorganisation des fichiers
    
    ---------
    
    Co-authored-by: Anselme <anselme@chorein.fr>

---
+++ packages/frontend-usagers/src/utils/agrementMenu.ts
@@ -0,0 +1,48 @@
+const menus = [
+  {
+    id: "agrement-coordonnees",
+    text: "Coordonnées à vérifier",
+  },
+  {
+    id: "agrement-dossier",
+    text: "Dossier de candidature",
+  },
+  {
+    id: "agrement-bilan",
+    text: "Bilan des 4 années précédentes",
+  },
+  {
+    id: "agrement-synthese",
+    text: "Synthèse",
+  },
+];
+
+const titleStart = "Agrément | ";
+const titleEnd = " | Vacances Adaptées Organisées";
+const getCurrentStepNumber = (key: any) => {
+  return menus.findIndex((m) => m.id === key) + 1;
+};
+
+const titles = () => {
+  const stepNumber = menus.length;
+  return {
+    "#agrement-coordonnees":
+      titleStart +
+      `étape ${getCurrentStepNumber("agrement-coordonnees")} sur ${stepNumber} | Coordonnées à vérifier` +
+      titleEnd,
+    "#agrement-dossier":
+      titleStart +
+      `étape ${getCurrentStepNumber("agrement-dossier")} sur ${stepNumber} | Dossier de candidature` +
+      titleEnd,
+    "#agrement-bilan":
+      titleStart +
+      `étape ${getCurrentStepNumber("agrement-bilan")} sur ${stepNumber} | Bilan des 4 années précédentes` +
+      titleEnd,
+    "#agrement-synthese":
+      titleStart +
+      `étape ${getCurrentStepNumber("agrement-synthese")} sur ${stepNumber} | Synthèse` +
+      titleEnd,
+  };
+};
+
+export default { menus, titles };
