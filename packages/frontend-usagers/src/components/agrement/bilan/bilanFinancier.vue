1f176e3d19d2cf829382c7a1614c4ab614389a13
admin <admin@MacBook-Pro-de-dum-admin.local>
2025-11-21 18:53:54 +0100

    feat: front usager etape 3 sur 4 (#1079)

--- packages/frontend-usagers/src/components/agrement/bilan/bilanFinancier.vue
+++ packages/frontend-usagers/src/components/agrement/bilan/bilanFinancier.vue
@@ -6,8 +6,73 @@
   >
     Bilan financier sur les 4 dernières années
   </TitleWithIcon>
+  <div class="fr-fieldset__element">
+    <div class="fr-col-12">
+      <DsfrInputGroup
+        name="changements"
+        label="Note d'information présentant les éventuelles améliorations ou changements apportés aux séjours (optionnel)"
+        :model-value="changements"
+        :label-visible="true"
+        :is-textarea="true"
+        :is-valid="changementsMeta.valid"
+        :error-message="changementsErrorMessage"
+        hint="De votre propre initiative ou suite aux observations des inspecteurs de l'action sanitaire et sociale, les médecins inspecteurs de santé publique ou les inspecteurs des agences régionales de santé ayant la qualité de médecin à l'issue des contrôles effectués au cours de l'agrément."
+        @update:model-value="onChangementsChange"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { useForm, useField } from "vee-validate";
+import * as yup from "yup";
+import { AGREMENT_STATUT } from "@vao/shared-bridge";
 import { TitleWithIcon } from "@vao/shared-ui";
+
+const props = defineProps({
+  initAgrement: { type: Object, required: true },
+});
+
+const requiredUnlessBrouillon = (schema) =>
+  schema.when("statut", {
+    is: (val) => val !== AGREMENT_STATUT.BROUILLON,
+    then: (schema) => schema.required("Champ obligatoire"),
+    otherwise: (schema) => schema.nullable(),
+  });
+
+const validationSchema = yup.object({
+  statut: yup.mixed().oneOf(Object.values(AGREMENT_STATUT)).required(),
+  changements: requiredUnlessBrouillon(
+    yup.string().min(20, "Merci de décrire au moins 20 caractères.").nullable(),
+  ),
+});
+
+const initialValues = {
+  statut: props.initAgrement.statut || AGREMENT_STATUT.BROUILLON,
+  changements: props.initAgrement.bilan?.changements || "",
+};
+
+// const { handleSubmit, meta } = useForm({
+const { handleSubmit } = useForm({
+  validationSchema,
+  initialValues,
+  validateOnMount: false,
+});
+
+const {
+  value: changements,
+  errorMessage: changementsErrorMessage,
+  handleChange: onChangementsChange,
+  meta: changementsMeta,
+} = useField("changements");
+
+const validateForm = async () => {
+  const result = await handleSubmit((values) => values)();
+  return result;
+};
+
+defineExpose({
+  validateForm,
+  // isValid: () => meta.value.valid, // Optionnel : pour vérifier la validité
+});
 </script>
--- packages/frontend-usagers/src/components/agrement/bilan/changements.vue
+++ packages/frontend-usagers/src/components/agrement/bilan/changements.vue
@@ -10,21 +10,38 @@
   <div class="fr-fieldset__element">
     <div class="fr-col-12">
       <DsfrInputGroup
-        name="changements"
+        name="noteInformation"
         label="Note d'information présentant les éventuelles améliorations ou changements apportés aux séjours (optionnel)"
-        :model-value="changements"
+        :model-value="noteInformation"
         :label-visible="true"
         :is-textarea="true"
-        :is-valid="changementsMeta.valid"
-        :error-message="changementsErrorMessage"
+        :is-valid="noteInformationMeta.valid"
+        :error-message="noteInformationErrorMessage"
         hint="De votre propre initiative ou suite aux observations des inspecteurs de l'action sanitaire et sociale, les médecins inspecteurs de santé publique ou les inspecteurs des agences régionales de santé ayant la qualité de médecin à l'issue des contrôles effectués au cours de l'agrément."
-        @update:model-value="onChangementsChange"
+        @update:model-value="onNoteInformationChange"
       />
     </div>
+  </div>
+  <div class="fr-fieldset__element">
+    <DsfrFileUpload
+      v-model="fileAttestationsRapatriement"
+      label="Ajouter des fichiers (optionnel)"
+      :modifiable="true"
+    />
+  </div>
+  <div class="fr-fieldset__element">
+    <DsfrCheckbox
+      v-model="bilanAucunChangementEvolution"
+      name="checkbox-required-custom"
+      label="Aucun changement ou évolution à déclarer."
+      :error-message="bilanAucunChangementEvolutionErrorMessage"
+      required
+    />
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { useForm, useField } from "vee-validate";
 import * as yup from "yup";
 import { AGREMENT_STATUT } from "@vao/shared-bridge";
@@ -34,23 +51,22 @@ const props = defineProps({
   initAgrement: { type: Object, required: true },
 });
 
-const requiredUnlessBrouillon = (schema) =>
-  schema.when("statut", {
-    is: (val) => val !== AGREMENT_STATUT.BROUILLON,
-    then: (schema) => schema.required("Champ obligatoire"),
-    otherwise: (schema) => schema.nullable(),
-  });
+const fileAttestationsRapatriement = ref([]);
 
 const validationSchema = yup.object({
   statut: yup.mixed().oneOf(Object.values(AGREMENT_STATUT)).required(),
-  changements: requiredUnlessBrouillon(
-    yup.string().min(20, "Merci de décrire au moins 20 caractères.").nullable(),
-  ),
+  noteInformation: yup
+    .string()
+    .min(20, "Merci de décrire au moins 20 caractères.")
+    .nullable(),
+  bilanAucunChangementEvolution: yup.boolean().required(),
 });
 
 const initialValues = {
   statut: props.initAgrement.statut || AGREMENT_STATUT.BROUILLON,
-  changements: props.initAgrement.bilan?.changements || "",
+  noteInformation: props.initAgrement.bilan?.noteInformation || "",
+  bilanAucunChangementEvolution:
+    props.initAgrement.bilan?.bilanAucunChangementEvolution || false,
 };
 
 // const { handleSubmit, meta } = useForm({
@@ -61,14 +77,27 @@ const { handleSubmit } = useForm({
 });
 
 const {
-  value: changements,
-  errorMessage: changementsErrorMessage,
-  handleChange: onChangementsChange,
-  meta: changementsMeta,
-} = useField("changements");
+  value: noteInformation,
+  errorMessage: noteInformationErrorMessage,
+  handleChange: onNoteInformationChange,
+  meta: noteInformationMeta,
+} = useField("noteInformation");
+
+const {
+  value: bilanAucunChangementEvolution,
+  errorMessage: bilanAucunChangementEvolutionErrorMessage,
+} = useField("bilanAucunChangementEvolution");
 
 const validateForm = async () => {
   const result = await handleSubmit((values) => values)();
+  if (result) {
+    return {
+      ...result,
+      ...(fileAttestationsRapatriement.value.length > 0 && {
+        fileAttestationsRapatriement: fileAttestationsRapatriement.value,
+      }),
+    };
+  }
   return result;
 };
 
