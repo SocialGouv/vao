5b8e8c311c198b9d00800edca62e5db35cd83912
l-scherer <159151890+l-scherer@users.noreply.github.com>
2025-12-11 15:39:27 +0100

    feat: 1274 agrement add adresse post and get (#1106)
    ## Ticket(s) lié(s)
    1274
    ## Description
    Ajout de l'adresse dans le DTO pour remplacer l'AdresseId par l'objet
    Adresse.
    Insert/ Update mis à jour
    
    ---------
    
    Co-authored-by: Anselme <anselme@chorein.fr>
    Co-authored-by: admin <admin@MacBook-Pro-de-dum-admin.local>

--- packages/shared-bridge/src/dto/index.ts
+++ packages/shared-bridge/src/dto/index.ts
@@ -4,6 +4,7 @@ export * from "./personnePhysiqueDto";
 export * from "./siret.dto";
 export * from "./userDto";
 /* eslint-disable import/no-unresolved */
+export * from "./adresse.dto";
 export * from "./agrement.dto";
 export * from "./document.dto";
 export * from "./hebergement.dto";
