8d3bb69b4254ba358215a615053769fcfc83ecf8
l-scherer <laurent.scherer@sg.social.gouv.fr>
2026-01-14 10:20:32 +0100

    feat: 1228 remove blocked test by ts

--- packages/shared-bridge/src/utils/siret.spec.ts
+++ packages/shared-bridge/src/utils/siret.spec.ts
@@ -10,9 +10,6 @@ describe("formatSiret", () => {
   it("should return input when it isn't a Siret lenght", () => {
     expect(formatSiret({ siret: "123456789" })).toBe("123456789");
   });
-  it("should return empty response when input is wrong object", () => {
-    expect(formatSiret({ siren: "12345678901234" })).toBe("");
-  });
   it("should format SIRET when input is a valid number", () => {
     expect(formatSiret({ siret: 12345678901234 })).toBe("123 456 789 01234");
   });
@@ -26,9 +23,6 @@ describe("formatSiren", () => {
   });
   it("should return entry when it isn't a Siren", () => {
     expect(formatSiren({ siren: "12345678901234" })).toBe("12345678901234");
-  });
-  it("should return empty response when input is wrong object", () => {
-    expect(formatSiren({ siret: "12345678901234" })).toBe("");
   });
   it("should format SIREN when input is a valid number", () => {
     expect(formatSiren({ siren: 123456789 })).toBe("123 456 789");
