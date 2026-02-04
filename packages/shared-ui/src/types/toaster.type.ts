84ceb816562425c6b15e2008bece29afb1387089
admin <admin@MacBook-Pro-de-dum-admin.local>
2026-02-04 12:17:22 +0100

    fix: composable pour les toasters 1261 (rm type any)

--- packages/shared-ui/src/types/toaster.type.ts
+++ packages/shared-ui/src/types/toaster.type.ts
@@ -14,7 +14,6 @@ export interface ToastOptions {
   closeButtonLabel?: string;
   duration?: number;
   role?: "alert" | "status";
-  [key: string]: any;
 }
 
 export interface Toast extends ToastOptions {
