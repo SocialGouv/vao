d8fb59f995612a40d67db7b144e393c0026fa5ce
olivier-rabot <167769451+olivier-rabot@users.noreply.github.com>
2026-02-16 13:38:42 +0100

    feat: 1223 add boutons tableau representants (rgaa) (#1186)
    ## Ticket(s) lié(s)
    
    https://jira-mcas.atlassian.net/jira/software/c/projects/VAO/boards/336?selectedIssue=VAO-1223
    
    ## Description
    Amelioration rgaa: ajout de bouton pour l'édition / suppression d'un
    representant legal
    
    ### Dont régressions potentielles à tester
    
    ## Screenshot / liens loom 
    
    ## Check-list
    
     - [x] Ma branche est rebase sur main
    - [ ] Des tests ont été écrits pour tous les endpoints créés ou modifiés
     - [ ] Refacto "à la volée" des parties sur lesquelles j'ai codée
     - [x] Plus de `console.log`
    - [ ] J'ai ajouté une validation de schéma sur la route que j'ai ajouté
    ou modifié
    
    ---------
    
    Co-authored-by: admin <admin@MacBook-Pro-de-dum-admin.local>

---
+++ packages/frontend-usagers/src/components/PersonneActionsCell.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul class="btns-group">
+    <li>
+      <DsfrButton
+        class="fr-icon-edit-line"
+        tertiary
+        no-outline
+        @click.stop="onEdit"
+      >
+        <span class="fr-sr-only">
+          Éditer {{ personne?.prenom || "" }} {{ personne?.nom || "" }}
+        </span>
+      </DsfrButton>
+    </li>
+
+    <li>
+      <DsfrButton
+        class="fr-icon-delete-fill"
+        tertiary
+        no-outline
+        @click.stop="onDelete"
+      >
+        <span class="fr-sr-only">
+          Supprimer {{ personne?.prenom || "" }} {{ personne?.nom || "" }}
+        </span>
+      </DsfrButton>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+type Personne = {
+  nom: string;
+  prenom: string;
+  fonction: string;
+};
+
+defineProps<{
+  personne: Personne;
+  onEdit: () => void;
+  onDelete: () => void;
+}>();
+</script>
+
+<style scoped>
+.btns-group {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-end;
+  gap: 0.5rem;
+  list-style: none;
+  padding: 0;
+}
+</style>
