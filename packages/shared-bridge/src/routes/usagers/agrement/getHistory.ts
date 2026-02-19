db4bfc5c493828c970e49046ea3099eeccfe2dec
admin <admin@MacBook-Pro-de-dum-admin.local>
2026-02-19 15:04:38 +0100

    fix: 1265 page historique renouvellement agrement (imports)

--- packages/shared-bridge/src/routes/usagers/agrement/getHistory.ts
+++ packages/shared-bridge/src/routes/usagers/agrement/getHistory.ts
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 import type { BasicRoute, RouteResponseBody, RouteSchema } from "../../..";
-import type { AgrementHistoryItem } from "../../../constantes/agrement";
+import type { AgrementHistoryItem } from "../../../dto/agrement.dto";
 
 export interface GetHistoryRoute extends BasicRoute {
   method: "GET";
