aca966010074267cf0ea35b61f05c68ff8e27ae2
olivierrabot <olivier.rabot@sg.social.gouv.fr>
2026-02-24 15:08:12 +0100

    feat: 1103 confirmation demande agrement (page statique)

--- packages/frontend-usagers/src/components/agrement/synthese.vue
+++ packages/frontend-usagers/src/components/agrement/synthese.vue
@@ -1,108 +1,107 @@
 <template>
   <div id="agrement-synthese">
     <DsfrAccordionsGroup v-model="expandedIndex">
-    <DsfrAccordion>
-      <template #title>
-        Coordonnées à vérifier
-        <DsfrBadge
-          :label="coordonneesValid ? 'Complet' : 'Incomplet'"
-          :type="coordonneesValid ? 'success' : 'warning'"
-          small
+      <DsfrAccordion>
+        <template #title>
+          Coordonnées à vérifier
+          <DsfrBadge
+            :label="coordonneesValid ? 'Complet' : 'Incomplet'"
+            :type="coordonneesValid ? 'success' : 'warning'"
+            small
+          />
+        </template>
+        <AgrementCoordonnees
+          v-model:valid="coordonneesValid"
+          :init-organisme="props.initOrganisme ?? {}"
+          :init-agrement="props.initAgrement ?? {}"
+          :modifiable="false"
+          :cdn-url="cdnUrl"
         />
-      </template>
-      <AgrementCoordonnees
-        v-model:valid="coordonneesValid"
-        :init-organisme="props.initOrganisme ?? {}"
-        :init-agrement="props.initAgrement ?? {}"
-        :modifiable="false"
-        :cdn-url="cdnUrl"
-      />
-      <DsfrButton 
-        class="fr-mb-6v fr-mt-6v" 
-        @click.prevent="onModifierCoordonnees"
-      >Modifier</DsfrButton
-    >
-    </DsfrAccordion>
-    <DsfrAccordion>
-      <template #title>
-        Dossier candidature
-        <DsfrBadge
-          :label="dossierValid ? 'Complet' : 'Incomplet'"
-          :type="dossierValid ? 'success' : 'warning'"
-          small
+        <DsfrButton
+          class="fr-mb-6v fr-mt-6v"
+          @click.prevent="onModifierCoordonnees"
+          >Modifier</DsfrButton
+        >
+      </DsfrAccordion>
+      <DsfrAccordion>
+        <template #title>
+          Dossier candidature
+          <DsfrBadge
+            :label="dossierValid ? 'Complet' : 'Incomplet'"
+            :type="dossierValid ? 'success' : 'warning'"
+            small
+          />
+        </template>
+        <AgrementDossier
+          v-model:valid="dossierValid"
+          class="fr-my-2w"
+          :init-agrement="props.initAgrement ?? {}"
+          :modifiable="false"
+          :cdn-url="cdnUrl"
         />
-      </template>
-      <AgrementDossier
-        v-model:valid="dossierValid"
-        class="fr-my-2w"
-        :init-agrement="props.initAgrement ?? {}"
-        :modifiable="false"
-        :cdn-url="cdnUrl"
-      />
-      <DsfrButton 
-        class="fr-mb-6v fr-mt-6v" 
-        @click.prevent="onModifierDossier"
-      >Modifier</DsfrButton>
-    </DsfrAccordion>
-    <DsfrAccordion>
-      <template #title>
-        Bilan des 4 années précédentes
-        <DsfrBadge
-          :label="bilanValid ? 'Complet' : 'Incomplet'"
-          :type="bilanValid ? 'success' : 'warning'"
-          small
+        <DsfrButton class="fr-mb-6v fr-mt-6v" @click.prevent="onModifierDossier"
+          >Modifier</DsfrButton
+        >
+      </DsfrAccordion>
+      <DsfrAccordion>
+        <template #title>
+          Bilan des 4 années précédentes
+          <DsfrBadge
+            :label="bilanValid ? 'Complet' : 'Incomplet'"
+            :type="bilanValid ? 'success' : 'warning'"
+            small
+          />
+        </template>
+        <AgrementBilan
+          v-model:valid="bilanValid"
+          :init-agrement="props.initAgrement ?? {}"
+          :modifiable="false"
+          :cdn-url="cdnUrl"
         />
-      </template>
-      <AgrementBilan
-        v-model:valid="bilanValid"
-        :init-agrement="props.initAgrement ?? {}"
-        :modifiable="false"
-        :cdn-url="cdnUrl"
-      />
-      <DsfrButton 
-        class="fr-mb-6v fr-mt-6v" 
-        @click.prevent="onModifierBilan"
-      >Modifier</DsfrButton>
-    </DsfrAccordion>
-    <DsfrAccordion>
-      <template #title>
-        Projet de séjours envisagés
-        <DsfrBadge
-          :label="projetValid ? 'Complet' : 'Incomplet'"
-          :type="projetValid ? 'success' : 'warning'"
-          small
+        <DsfrButton class="fr-mb-6v fr-mt-6v" @click.prevent="onModifierBilan"
+          >Modifier</DsfrButton
+        >
+      </DsfrAccordion>
+      <DsfrAccordion>
+        <template #title>
+          Projet de séjours envisagés
+          <DsfrBadge
+            :label="projetValid ? 'Complet' : 'Incomplet'"
+            :type="projetValid ? 'success' : 'warning'"
+            small
+          />
+        </template>
+        <AgrementProjets
+          v-model:valid="projetValid"
+          :init-agrement="props.initAgrement ?? {}"
+          :modifiable="false"
+          :cdn-url="cdnUrl"
         />
-      </template>
-      <AgrementProjets
-        v-model:valid="projetValid"
-        :init-agrement="props.initAgrement ?? {}"
-        :modifiable="false"
-        :cdn-url="cdnUrl"
-      />
-      <DsfrButton 
-        class="fr-mb-6v fr-mt-6v" 
-        @click.prevent="onModifierProjet"
-      >Modifier</DsfrButton>
-    </DsfrAccordion>
-    <DsfrAlert role="info">
-      <h2>Vérification</h2>
-      <ul role="list">
-        <li role="listitem">
-          Vérifiez que toutes les informations soient exactes.
-        </li>
-        <li role="listitem">
-          Assurez-vous que tous les documents soient lisibles.
-        </li>
-        <li role="listitem">
-          Toutes les étapes du formulaire doivent être marquées comme « Complet
-          » pour pouvoir être envoyées
-        </li>
-      </ul>
-    </DsfrAlert>
+        <DsfrButton class="fr-mb-6v fr-mt-6v" @click.prevent="onModifierProjet"
+          >Modifier</DsfrButton
+        >
+      </DsfrAccordion>
+      <DsfrAlert role="info">
+        <h2>Vérification</h2>
+        <ul role="list">
+          <li role="listitem">
+            Vérifiez que toutes les informations soient exactes.
+          </li>
+          <li role="listitem">
+            Assurez-vous que tous les documents soient lisibles.
+          </li>
+          <li role="listitem">
+            Toutes les étapes du formulaire doivent être marquées comme «
+            Complet » pour pouvoir être envoyées
+          </li>
+        </ul>
+      </DsfrAlert>
     </DsfrAccordionsGroup>
-    <DsfrButton 
-      class="fr-mb-6v fr-mt-6v" 
-      :disabled="!(coordonneesValid && projetValid && dossierValid && bilanValid)"
+    <DsfrButton
+      class="fr-mb-6v fr-mt-6v"
+      :disabled="
+        !(coordonneesValid && projetValid && dossierValid && bilanValid)
+      "
       @click.prevent="onNext"
       >Confirmer ma demande d'agrément</DsfrButton
     >
@@ -123,9 +122,9 @@ const dossierValid = ref(false);
 const bilanValid = ref(false);
 const projetValid = ref(false);
 
-
 function onNext() {
   // TO DO TICKET 1103
+  navigateTo("/demande-agrement-transmise");
 }
 function onModifierCoordonnees() {
   navigateTo("/agrement#agrement-coordonnees");
---
+++ packages/frontend-usagers/src/pages/demande-agrement-transmise/index.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="fr-container">
+    <DsfrBreadcrumb :links="links" />
+    <h1>
+      <span
+        aria-hidden="true"
+        class="fr-icon--green fr-icon-checkbox-line fr-pr-2v"
+      ></span
+      >Demande de renouvellement d’agrément soumise avec succès
+    </h1>
+    <p>Votre demande a été transmise à la DREETS (Direction régionale de l'économie, de l'emploi, du travail et des solidarités) de la région de votre siège social.</p>
+    <p>Un accusé de réception a été envoyé à {{ userStore.user?.email ?? "votre adresse email" }}.</p>
+    <div class="flex">
+      <div><img src="/assets/mon-agrement.svg" alt="" aria-hidden="true" /></div>
+      <div class="fr-pt-10v">
+        <h2>Prochaine étape : Analyse de votre dossier.</h2>
+        <h3 class="fr-mb-2v">Suivez l’avancement de votre dossier depuis votre page Agrément.</h3>
+          <NuxtLink
+            to="/mon-agrement"
+            class="fr-btn fr-btn--primary"
+          >
+            Accéder à ma page Agrément
+          </NuxtLink>
+          <p class="fr-mt-6v"><span class="fr-icon-mail-line fr-pr-2v" aria-hidden="true"></span>Vous recevrez un e-mail à chaque étape.</p>
+      </div>
+    </div>
+  </div>
+
+  <AgrementAideContact />
+</template>
+
+<script setup lang="ts">
+const userStore = useUserStore();
+
+useHead({
+  title: "Demande d'agrément transmise | Vacances Adaptées Organisées",
+  meta: [
+    {
+      name: "description",
+      content: "Demande d'agrément transmise.",
+    },
+  ],
+});
+
+const links = [
+  {
+    to: "/",
+    text: "Accueil",
+  },
+  {
+    text: "Renouvellement d'agrément",
+  },
+];
+
+
+</script>
+<style scoped>
+.fr-icon--green {
+  color: var(--dark-decisions-text-text-default-success, #27a658);
+}
+h1 {
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 40px;
+}
+h2 {
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 32px;
+}
+h3 {
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 28px;
+}
+.flex {
+  min-width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 46px;
+}
+@media (max-width: 768px) {
+  .flex {
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+  }
+}
+</style>
