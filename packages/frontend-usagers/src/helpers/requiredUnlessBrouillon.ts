b86456e46b972c7596f85856e45752b1bde9112d
Anselme <anselme@chorein.fr>
2026-03-02 15:53:08 +0100

    fix: lint pgpool and requiredUnlessBrouillon

--- packages/frontend-usagers/src/helpers/requiredUnlessBrouillon.ts
+++ packages/frontend-usagers/src/helpers/requiredUnlessBrouillon.ts
@@ -1,9 +1,9 @@
-import * as yup from "yup";
+import type { AnySchema } from "yup";
 import { AGREMENT_STATUT } from "@vao/shared-bridge";
 
-export const requiredUnlessBrouillon = (schema: yup.AnySchema) =>
+export const requiredUnlessBrouillon = (schema: AnySchema) =>
   schema.when("statut", {
     is: (val: AGREMENT_STATUT) => val !== AGREMENT_STATUT.BROUILLON,
-    then: (schema: any) => schema.required("Champ obligatoire"),
-    otherwise: (schema: any) => schema.nullable(),
+    then: (schema: AnySchema) => schema.required("Champ obligatoire"),
+    otherwise: (schema: AnySchema) => schema.nullable(),
   });
