2f7df5a7e0130083371c1200772522cbb71fbdf5
l-scherer <laurent.scherer@sg.social.gouv.fr>
2026-03-31 10:00:58 +0200

    feat: 1361 fusager I give my opinion

---
+++ packages/frontend-usagers/src/components/demande-sejour/ConfirmTransmission.vue
@@ -0,0 +1,59 @@
+<template>
+  <h2 class="fr-h4">
+    <span
+      class="fr-icon-check-line fr-icon--lg fr-mr-1w icon-success"
+      aria-hidden="true"
+      style="color: var(--text-default-success)"
+    ></span>
+    Votre déclaration a bien été transmise
+  </h2>
+
+  <section class="fr-container fr-my-4w">
+    <p>Votre demande est en cours de traitement</p>
+
+    <div class="avis-box" role="region" aria-labelledby="avis-title">
+      <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle">
+        <div class="fr-col-12 fr-col-md-6">
+          <h3 id="avis-title" class="fr-h4">
+            Comment s’est passée votre démarche ?
+          </h3>
+
+          <p class="fr-text--sm">
+            Aidez-nous à améliorer cette démarche en donnant votre avis, cela ne
+            prend que 2 minutes.
+          </p>
+        </div>
+        <div class="fr-col-12 fr-col-md-6 fr-text--right">
+          <a
+            href="https://jedonnemonavis.numerique.gouv.fr/Demarches/4077?button=4503"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Je donne mon avis - nouvelle fenêtre"
+          >
+            <img
+              target="_blank"
+              src="https://jedonnemonavis.numerique.gouv.fr/static/bouton-bleu-clair.svg"
+              alt="Je donne mon avis"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.icon-success {
+  color: var(--text-default-success);
+}
+.avis-box {
+  background-color: #ececfe;
+  padding: 1rem;
+  margin: 1rem;
+}
+a img {
+  width: 90%;
+}
+</style>
--- packages/frontend-usagers/src/pages/demande-sejour/[[declarationId]].vue
+++ packages/frontend-usagers/src/pages/demande-sejour/[[declarationId]].vue
@@ -168,6 +168,16 @@
               </div>
             </div>
           </div>
+          <DsfrModal
+            ref="modal"
+            class="fr-icon-checkbox-circle-line fr-icon--lg"
+            name="modal-confirm-transmission"
+            :opened="isModalConfirmTransmissionOpened"
+            size="md"
+            @close="onCloseConfirmTransmissionModal"
+          >
+            <DemandeSejourConfirmTransmission />
+          </DsfrModal>
         </div>
       </DsfrTabContent>
 
@@ -241,7 +251,12 @@
 
 <script setup>
 import dayjs from "dayjs";
-import { Chat, DemandeStatusBadge, fileUtils, useToaster } from "@vao/shared-ui";
+import {
+  Chat,
+  DemandeStatusBadge,
+  fileUtils,
+  useToaster,
+} from "@vao/shared-ui";
 const getFileUploadErrorMessage = fileUtils.getFileUploadErrorMessage;
 
 const route = useRoute();
@@ -475,6 +490,12 @@ const hash = computed(() => {
   return sommaireOptions[0];
 });
 
+const isModalConfirmTransmissionOpened = ref(false);
+
+function onCloseConfirmTransmissionModal() {
+  isModalConfirmTransmissionOpened.value = false;
+  navigateTo("/demande-sejour/liste");
+}
 const canModify = computed(() => {
   return (
     !demandeCourante.value.statut ||
@@ -663,12 +684,10 @@ async function updateOrCreate(data, type) {
       },
     });
 
-    toaster.success(
-      {
-        titleTag: "h2",
-        description: `Déclaration de séjour ${sejourId.value ? "sauvegardée" : "créée"}`,
-      }
-    );
+    toaster.success({
+      titleTag: "h2",
+      description: `Déclaration de séjour ${sejourId.value ? "sauvegardée" : "créée"}`,
+    });
     log.d(`demande de séjour ${sejourId.value} mis à jour`);
     sejourId.value = response.id;
     return await nextHash();
@@ -735,7 +754,7 @@ async function finalize(attestation) {
       });
     }
     log.d(`demande de séjour ${sejourId.value} transmise`);
-    return await navigateTo("/demande-sejour/liste");
+    isModalConfirmTransmissionOpened.value = true;
   } catch (error) {
     log.w("Finalisation de la declaration de sejour : ", { error });
     const codeError = error?.data?.name;
@@ -781,5 +800,8 @@ onMounted(async () => {
   display: flex;
   justify-content: end;
   align-items: start;
+}
+a img {
+  display: block;
 }
 </style>
