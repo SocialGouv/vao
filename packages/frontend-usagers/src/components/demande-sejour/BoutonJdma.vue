b981ffd456bc107fcd3d4e34517a94877e4bac4c
l-scherer <laurent.scherer@sg.social.gouv.fr>
2026-04-14 14:47:58 +0200

    feat: 1361 fusager add jdma list ds

---
+++ packages/frontend-usagers/src/components/demande-sejour/BoutonJdma.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fr-col-12 fr-col-md-6 fr-text--right">
+    <a
+      href="https://jedonnemonavis.numerique.gouv.fr/Demarches/4077?button=4503"
+      rel="noopener noreferrer"
+      title="Je donne mon avis"
+      target="_blank"
+    >
+      <img
+        src="https://jedonnemonavis.numerique.gouv.fr/static/bouton-bleu-clair.svg"
+        alt="Je donne mon avis"
+        style="display: block"
+      />
+    </a>
+  </div>
+</template>
