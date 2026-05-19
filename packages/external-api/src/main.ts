673a97c099b4b7b2c53c0f350fb2a9c77e329609
Anselme <anselme@chorein.fr>
2026-05-19 14:45:59 +0200

    chore: add security http header

--- packages/external-api/src/main.ts
+++ packages/external-api/src/main.ts
@@ -11,6 +11,16 @@ patchNestJsSwagger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.use((_req, res, next) => {
+    res.setHeader("Referrer-Policy", "no-referrer");
+    res.setHeader("X-XSS-Protection", "0");
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader(
+      "Content-Security-Policy",
+      "default-src 'self'; object-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'",
+    );
+    next();
+  });
   app.useLogger(app.get(Logger));
   const config = new DocumentBuilder()
     .setTitle("VAO Api")
