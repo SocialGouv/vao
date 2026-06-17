52c999b2cafc3d90ad25d53208e6571874774b5f
l-scherer <159151890+l-scherer@users.noreply.github.com>
2026-06-17 09:04:59 +0200

    fix: 1403 fo wrong siret loading first (#1412)
    ## Ticket(s) lié(s)
    1403
    
    ## Description
    Chargement du siret dans le bon ordre pour pouvoir comparer du plus
    récent au plus ancien

--- packages/frontend-usagers/src/components/demande-sejour/table.vue
+++ packages/frontend-usagers/src/components/demande-sejour/table.vue
@@ -180,9 +180,9 @@ const title = "Liste des séjours";
 
 const siretUser = computed(
   () =>
-    userStore.user?.userSiret ??
     organismeStore.organismeCourant?.personneMorale?.siret ??
-    organismeStore.organismeCourant?.personnePhysique?.siret,
+    organismeStore.organismeCourant?.personnePhysique?.siret ??
+    userStore.user?.userSiret,
 );
 
 const draftStatus = statusUtils.defaultStatus.BROUILLON;
